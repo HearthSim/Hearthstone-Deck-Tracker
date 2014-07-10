c5cc9b83b02421815a2da5b481b353644cbd66bc
epix37 <epikz37@gmail.com>
2014-07-10 19:14:48 +0200

    manabar animations

--- Hearthstone Deck Tracker/Controls/ManaCostBar.xaml.cs
+++ Hearthstone Deck Tracker/Controls/ManaCostBar.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
 
 namespace Hearthstone_Deck_Tracker
 {
@@ -7,26 +10,119 @@ namespace Hearthstone_Deck_Tracker
     /// </summary>
     public partial class ManaCostBar
     {
+        private readonly double[] _previousBarHeights;
+        private readonly Rectangle[] _bars;
+        private readonly Queue<double[]> _animationQueue;
+        private bool _isAnimationRunning;
+        public int AnimationDuration { get; set; }
+        public int FrameDelay { get; set; }
+
         public ManaCostBar()
         {
             InitializeComponent();
+            _previousBarHeights = new double[] {0.0, 0.0, 0.0};
+            _bars = new Rectangle[] {WeaponsRect, SpellsRect, MinionsRect};
+            AnimationDuration = 500;
+            FrameDelay = 20;
+            _animationQueue = new Queue<double[]>();
+            _isAnimationRunning = false;
         }
 
-        public void SetValues(double first, double second, double third, int count)
+        
+        public void SetValues(double weapons, double spells, double minions, int count)
         {
-            WeaponsRect.Height = ActualHeight * first / 100;
-            SpellsRect.Height = ActualHeight * second / 100;
-            MinionsRect.Height = ActualHeight * third / 100;
-
             LabelCount.Content = count;
 
-            var offset = TotalHeight - 22;
-            if (offset < -4) offset = -4;
-            LabelCount.Margin = new Thickness(0, 0, 0, offset);
+            _animationQueue.Enqueue(new double[] {ActualHeight * weapons / 100, ActualHeight * spells / 100, ActualHeight * minions / 100});
+
+            if (!_isAnimationRunning)
+                Animate();
         }
-        public double TotalHeight
+
+        private bool AnimateBar(Rectangle bar, double from, double to)
         {
-            get { return WeaponsRect.Height + SpellsRect.Height + MinionsRect.Height; } 
+            if (to > from)
+            {
+                if (bar.Height < to)
+                {
+                    bar.Height += (ActualHeight * FrameDelay) / AnimationDuration;
+                    if (bar.Height > to)
+                    {
+                        bar.Height = to;
+                        return true;
+                    }
+                }
+                else
+                {
+                    bar.Height = to;
+                    return true;
+                }
+            }
+            else if (to < from)
+            {
+                if (bar.Height > to)
+                {
+                    var newHeight = bar.Height - (ActualHeight * FrameDelay) / AnimationDuration;
+                    if (newHeight < to || newHeight < 0)
+                    {
+                        bar.Height = to;
+                        return true;
+                    }
+
+                    bar.Height -= (ActualHeight * FrameDelay) / AnimationDuration;
+                }
+                else
+                {
+                    bar.Height = to;
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+            return false;
         }
+
+        private async void Animate()
+        {
+            _isAnimationRunning = true;
+
+            while(_animationQueue.Count > 0)
+            {
+                var targetValues = _animationQueue.Dequeue();
+
+                bool[] done = {false, false, false};
+
+                if (double.IsNaN(targetValues[0]) || targetValues[0] < 0)
+                    targetValues[0] = 0.0;
+                if (double.IsNaN(targetValues[1]) || targetValues[1] < 0)
+                    targetValues[1] = 0.0;
+                if (double.IsNaN(targetValues[2]) || targetValues[2] < 0)
+                    targetValues[2] = 0.0;
+
+                while (!done[0] || !done[1] || !done[2])
+                {
+                    //minions first, weapons last
+                    if (!done[2])
+                        done[2] = AnimateBar(_bars[2], _previousBarHeights[2], targetValues[2]);
+
+                    if (!done[1])
+                        done[1] = AnimateBar(_bars[1], _previousBarHeights[1], targetValues[1]);
+
+                    if (!done[0])
+                        done[0] = AnimateBar(_bars[0], _previousBarHeights[0], targetValues[0]);
+                
+                    await Task.Delay(FrameDelay);
+                }
+
+                _previousBarHeights[0] = targetValues[0];
+                _previousBarHeights[1] = targetValues[1];
+                _previousBarHeights[2] = targetValues[2];
+            }
+
+            _isAnimationRunning = false;
+        }
+
     }
 }
