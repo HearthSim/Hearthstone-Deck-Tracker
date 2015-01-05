175e49e56c00b1383a38312851458a601da76b8a
epix37 <epikz37@gmail.com>
2015-01-05 19:04:36 +0100

    fixed IsMulliganDone (caused crash) (#526)

--- Hearthstone Deck Tracker/Hearthstone/Game.cs
+++ Hearthstone Deck Tracker/Hearthstone/Game.cs
@@ -106,14 +106,17 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 						   : "enUS");
 		}
 
+
 		public static bool IsMulliganDone
 		{
 			get
 			{
-				if(Entities.Count < 3)
+				var player = Entities.FirstOrDefault(x => x.Value.IsPlayer);
+				var opponent = Entities.FirstOrDefault(x => x.Value.HasTag(GAME_TAG.PLAYER_ID) && !x.Value.IsPlayer);
+				if(player.Value == null || opponent.Value == null)
 					return false;
-				return Entities[2].GetTag(GAME_TAG.MULLIGAN_STATE) == (int)TAG_MULLIGAN.DONE &&
-				       Entities[3].GetTag(GAME_TAG.MULLIGAN_STATE) == (int)TAG_MULLIGAN.DONE;
+				return player.Value.GetTag(GAME_TAG.MULLIGAN_STATE) == (int)TAG_MULLIGAN.DONE &&
+					   opponent.Value.GetTag(GAME_TAG.MULLIGAN_STATE) == (int)TAG_MULLIGAN.DONE;
 			}
 		}
 
