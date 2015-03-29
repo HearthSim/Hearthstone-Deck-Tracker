f241ad09bef8e482204b970b9183674f1eb597a7
epix37 <epikz37@gmail.com>
2015-03-29 12:07:33 +0200

    replayviewer: turnitem list tooltips (#804)

--- Hearthstone Deck Tracker/Replay/ReplayKeyPoint.cs
+++ Hearthstone Deck Tracker/Replay/ReplayKeyPoint.cs
@@ -1,5 +1,6 @@
 #region
 
+using System;
 using System.Linq;
 using Hearthstone_Deck_Tracker.Enums;
 using Hearthstone_Deck_Tracker.Hearthstone;
@@ -30,6 +31,7 @@ namespace Hearthstone_Deck_Tracker.Replay
 			get { return Data[0].GetTag(GAME_TAG.TURN); }
 		}
 
+		[Obsolete]
 		public override string ToString()
 		{
 			var additionalInfo = "";
@@ -71,45 +73,17 @@ namespace Hearthstone_Deck_Tracker.Replay
 			return string.Format("{1} {2}", Player, Type, additionalInfo);
 		}
 
+		public string GetCardId()
+		{
+			var id = Type == KeyPointType.Attack ? Data[0].GetTag(GAME_TAG.PROPOSED_ATTACKER) : Id;
+			var entity = Data.FirstOrDefault(x => x.Id == id);
+			return entity != null ? entity.CardId : null;
+		}
+
 		public string GetAdditionalInfo()
 		{
-			var additionalInfo = "";
-			if(Type == KeyPointType.Attack)
-			{
-				var attackerId = Data[0].GetTag(GAME_TAG.PROPOSED_ATTACKER);
-				var attackerCardId = Data.First(x => x.Id == attackerId).CardId;
-				if(!string.IsNullOrEmpty(attackerCardId))
-					additionalInfo += Game.GetCardFromId(attackerCardId).LocalizedName;
-
-				//additionalInfo += " -> ";
-
-				//var defenderId = Data[0].GetTag(GAME_TAG.PROPOSED_DEFENDER);
-				//var defenderCardId = Data.First(x => x.Id == defenderId).CardId;
-				//if(!string.IsNullOrEmpty(defenderCardId))
-				//	additionalInfo += Game.GetCardFromId(defenderCardId).LocalizedName;
-			}
-			else if(Type == KeyPointType.PlaySpell)
-			{
-				var entity = Data.First(x => x.Id == Id);
-				if(!string.IsNullOrEmpty(entity.CardId))
-					additionalInfo += Game.GetCardFromId(entity.CardId).LocalizedName;
-
-				//additionalInfo += " -> ";
-
-				//var targetId = entity.GetTag(GAME_TAG.CARD_TARGET);
-				//var targetCardId = Data.First(x => x.Id == targetId).CardId;
-				//if(!string.IsNullOrEmpty(targetCardId))
-				//	additionalInfo += Game.GetCardFromId(targetCardId).LocalizedName;
-			}
-			else
-			{
-				var entityCardId = Data.First(x => x.Id == Id).CardId;
-				if(!string.IsNullOrEmpty(entityCardId))
-					additionalInfo = Game.GetCardFromId(entityCardId).LocalizedName;
-				else
-					additionalInfo = "Entity " + Id;
-			}
-			return additionalInfo;
+			var cardId = GetCardId();
+			return string.IsNullOrEmpty(cardId) ? "Entity " + Id : Game.GetCardFromId(GetCardId()).LocalizedName;
 		}
 	}
 }
