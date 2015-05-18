42001023b5be2730ebb9172048bf771db3bd1d2b
epix37 <epikz37@gmail.com>
2015-05-18 19:24:50 +0200

    exposes Overlay.CanvasInfo in API.Overlay

---
+++ Hearthstone Deck Tracker/API/Overlay.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using System.Windows.Controls;
+
+#endregion
+
+namespace Hearthstone_Deck_Tracker.API
+{
+	public class Overlay
+	{
+		public Canvas OverlayCanvas
+		{
+			get { return Helper.MainWindow.Overlay.CanvasInfo; }
+		}
+	}
+}
