a4e0869ff9e5798ec7505499f077a0a43fce6e04
Andrew Burnett <andrew@nospam.forme>
2015-07-23 22:09:11 +0100

    Fix Brawl cards loading from DB and new hero skins (#1141, #1165)
    Reversed ValidCardSets to InvalidCardSets to better handle future updates. Added some invalid card ids from brawl set, for "actual cards". Some small tests added to check logic is correct.

---
+++ HDTTests/Hearthstone/CardDbTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HDTTests.Hearthstone
+{
+	[TestClass]
+	public class CardDBTest
+	{
+		/*
+		 *  Correct for patch 2.7.0.9166
+		 */
+
+		[TestMethod]
+		public void TestTotalCollectableCards()
+		{
+			Assert.AreEqual(566, Game.GetActualCards().Count);
+		}
+
+		[TestMethod]
+		public void TestHeroSkins()
+		{
+			var Alleria = Game.GetHeroNameFromId("HERO_05a");
+			Assert.AreEqual("Hunter", Alleria);
+
+			var AlleriaPower = Game.GetCardFromId("DS1h_292_H1");
+			Assert.AreEqual("Steady Shot", AlleriaPower.Name);
+		}
+
+		[TestMethod]
+		public void TestBrawlCards()
+		{
+			var Rotten = Game.GetCardFromId("TB_008");
+			Assert.AreEqual("Rotten Banana", Rotten.Name);
+
+			var Moira = Game.GetCardFromId("BRMC_87");
+			Assert.AreEqual("Moira Bronzebeard", Moira.Name);
+		}
+	}
+}
