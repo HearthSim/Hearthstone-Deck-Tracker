96d26a5142e230aac9940e8358a39486e5a07120
Zachary Lewis <zachwlewis@gmail.com>
2015-08-26 05:34:15 -0500

    I can live with 26.

--- Hearthstone Deck Tracker/MainWindow/MainWindow.xaml.cs
+++ Hearthstone Deck Tracker/MainWindow/MainWindow.xaml.cs
@@ -1088,7 +1088,7 @@ namespace Hearthstone_Deck_Tracker
 						if(_currentNewsId > oldNewsId
 						   || StatusBarNews.Visibility == Visibility.Collapsed && _currentNewsId > Config.Instance.IgnoreNewsId)
 						{
-							TopRow.Height = new GridLength(30);
+							TopRow.Height = new GridLength(26);
 							StatusBarNews.Visibility = Visibility.Visible;
 							MinHeight += StatusBarNewsHeight;
 							UpdateNews(0);
