4624bd2edcc168915e426eafd16bcec7d5121fc8
KonstantinVasilev <kostya-bko@mail.ru>
2015-09-06 22:04:55 +0700

    Add last played date for decks in deck select view.

---
+++ Hearthstone Deck Tracker/Controls/DeckPicker/DateConverter.cs
@@ -0,0 +1,36 @@
+﻿#region
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+#endregion
+
+namespace Hearthstone_Deck_Tracker.Controls.DeckPicker
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                var date = (DateTime)value;
+
+                if (date == DateTime.MinValue)
+                {
+                    return "N/A";
+                }
+                return date.ToString("d", culture);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- Hearthstone Deck Tracker/Hearthstone/Deck.cs
+++ Hearthstone Deck Tracker/Hearthstone/Deck.cs
@@ -302,7 +302,7 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 			get
 			{
 				var games = DeckStats.Games;
-				return !games.Any() ? DateTime.MinValue : games.OrderByDescending(g => g.StartTime).First().StartTime;
+				return !games.Any() ? DateTime.MinValue : games.Max(g => g.StartTime);
 			}
 		}
 
@@ -312,7 +312,7 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 			get
 			{
 				var games = DeckStats.Games;
-				return !games.Any() ? LastEdited : games.OrderByDescending(g => g.StartTime).First().StartTime;
+				return !games.Any() ? LastEdited : games.Max(g => g.StartTime);
 			}
 		}
 
