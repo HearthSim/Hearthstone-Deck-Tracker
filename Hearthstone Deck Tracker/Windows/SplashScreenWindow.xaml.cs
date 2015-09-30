80be61da8a25e12c0471fa737de94606902fbb09
Epix37 <epikz37@gmail.com>
2015-09-30 09:20:47 +0200

    fix property for C# 5

--- Hearthstone Deck Tracker/Windows/SplashScreenWindow.xaml.cs
+++ Hearthstone Deck Tracker/Windows/SplashScreenWindow.xaml.cs
@@ -1,28 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Hearthstone_Deck_Tracker.Annotations;
-
-namespace Hearthstone_Deck_Tracker.Windows
+﻿namespace Hearthstone_Deck_Tracker.Windows
 {
     /// <summary>
     /// Interaction logic for SplashScreenWindow.xaml
     /// </summary>
     public partial class SplashScreenWindow
     {
-        public string VersionString { get; }
+        public string VersionString { get; private set; }
 
         public SplashScreenWindow()
         {
