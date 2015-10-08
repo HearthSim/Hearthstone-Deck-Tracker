91333698b099faf82e33fdd3dab6f6b0c2bb43b3
Epix37 <epikz37@gmail.com>
2015-10-08 21:42:55 +0200

    update email

--- Hearthstone Deck Tracker/App.xaml.cs
+++ Hearthstone Deck Tracker/App.xaml.cs
@@ -52,7 +52,7 @@ namespace Hearthstone_Deck_Tracker
 
 			MessageBox.Show(
 			                "A crash report file was created at:\n\"" + Environment.CurrentDirectory + "\\" + fileName
-			                + ".txt\"\n\nPlease \na) create an issue on github (https://github.com/Epix37/Hearthstone-Deck-Tracker) \nor \nb) send me an email (epikz37@gmail.com).\n\nPlease include the generated crash report(s) and a short explanation of what you were doing before the crash.",
+			                + ".txt\"\n\nPlease \na) create an issue on github (https://github.com/Epix37/Hearthstone-Deck-Tracker) \nor \nb) send an email to support@hsdecktracker.net.\n\nPlease include the generated crash report(s) and a short explanation of what you were doing before the crash.",
 			                "Oops! Something went wrong.", MessageBoxButton.OK, MessageBoxImage.Error);
 			e.Handled = true;
 			Shutdown();
