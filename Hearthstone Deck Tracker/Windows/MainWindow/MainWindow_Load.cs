d01f00fc918e81a1ff3049c9a2cc43014b25f6e6
Jun Wu <quark@lihdd.net>
2015-10-19 18:38:31 +0800

    UseAnimations affects flyouts as well

--- Hearthstone Deck Tracker/Windows/MainWindow/MainWindow_Load.cs
+++ Hearthstone Deck Tracker/Windows/MainWindow/MainWindow_Load.cs
@@ -13,6 +13,7 @@ using MahApps.Metro;
 using MahApps.Metro.Controls.Dialogs;
 using Application = System.Windows.Application;
 using Point = System.Drawing.Point;
+using System.Collections.Generic;
 
 #endregion
 
@@ -135,9 +136,38 @@ namespace Hearthstone_Deck_Tracker.Windows
 				Helper.DpiScalingX = presentationsource.CompositionTarget.TransformToDevice.M11;
 				Helper.DpiScalingY = presentationsource.CompositionTarget.TransformToDevice.M22;
 			}
-		    LoadHearthStatsMenu();
-            LoadAndUpdateDecks(); 
-        }
+			LoadHearthStatsMenu();
+			LoadAndUpdateDecks();
+			UpdateFlyoutAnimationsEnabled();
+		}
+
+		public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj != null)
+			{
+				for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+				{
+					DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+					if (child != null && child is T)
+					{
+						yield return (T)child;
+					}
+
+					foreach (T childOfChild in FindVisualChildren<T>(child))
+					{
+						yield return childOfChild;
+					}
+				}
+			}
+		}
+
+		public void UpdateFlyoutAnimationsEnabled()
+		{
+			foreach (var flyout in FindVisualChildren<MahApps.Metro.Controls.Flyout>(Core.MainWindow))
+			{
+				flyout.AreAnimationsEnabled = Config.Instance.UseAnimations;
+			}
+		}
 
 		internal async Task<bool> SetupProtocol()
 		{
