735a8c525772f3b4bf8ad7225e408bc9c0a5b4ca
Epix37 <epikz37@gmail.com>
2015-10-26 22:35:54 +0100

    fix ArenaRewardPacks typo, remove ArenaRewards dialog dust tooltip (#1663)

--- Hearthstone Deck Tracker/Stats/CompiledStats/ArenaStats.cs
+++ Hearthstone Deck Tracker/Stats/CompiledStats/ArenaStats.cs
@@ -53,7 +53,7 @@ namespace Hearthstone_Deck_Tracker.Stats.CompiledStats
 
 		public int PacksCountGvg
 		{
-			get { return GetFilteredRuns().Sum(x => x.Packs.Count(p => p == ArenaRewardPacks.GoblingVsGnomes)); }
+			get { return GetFilteredRuns().Sum(x => x.Packs.Count(p => p == ArenaRewardPacks.GoblinsVsGnomes)); }
 		}
 
 		public int PacksCountTgt
