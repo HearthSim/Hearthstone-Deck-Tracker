198d3a62aa4af4bc8f9b687b0bc3dc730347e753
Alexander Zeier <epikz37@gmail.com>
2016-02-17 22:44:36 +0100

    hide flavor text tool tip in menu and when hearthstone is in background

--- Hearthstone Deck Tracker/Windows/OverlayWindow.Update.cs
+++ Hearthstone Deck Tracker/Windows/OverlayWindow.Update.cs
@@ -42,8 +42,10 @@ namespace Hearthstone_Deck_Tracker.Windows
 			var playerBoard =
 				Core.Game.Player.Board.Where(x => x.Entity.IsMinion).OrderBy(x => x.Entity.GetTag(GAME_TAG.ZONE_POSITION)).ToList();
 			UpdateMouseOverDetectionRegions(oppBoard, playerBoard);
-			if(User32.IsHearthstoneInForeground())
+			if(!_game.IsInMenu && User32.IsHearthstoneInForeground())
 				DetectMouseOver(playerBoard, oppBoard);
+			else
+				FlavorTextVisibility = Collapsed;
 
 			StackPanelPlayer.Opacity = Config.Instance.PlayerOpacity / 100;
 			StackPanelOpponent.Opacity = Config.Instance.OpponentOpacity / 100;
