8bda49376a6bab07427cfde70801820ccd31b039
Alexander Zeier <epikz37@gmail.com>
2016-02-18 14:00:10 +0100

    add LegendRank to GameMetaData

--- Hearthstone Deck Tracker/Hearthstone/GameMetaData.cs
+++ Hearthstone Deck Tracker/Hearthstone/GameMetaData.cs
@@ -12,8 +12,9 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 		public string ClientId { get; set; }
 		public string GameId { get; set; }
 		public string SpectateKey { get; set; }
+		public int LegendRank { get; set; }
 
 		public override string ToString() 
-			=> $"ServerAddress={ServerAddress}, ClientId={ClientId}, GameId={GameId}, SpectateKey={SpectateKey}";
+			=> $"ServerAddress={ServerAddress}, ClientId={ClientId}, GameId={GameId}, SpectateKey={SpectateKey}, LegendRank={LegendRank}";
 	}
 }
--- Hearthstone Deck Tracker/LogReader/Handlers/BobHandler.cs
+++ Hearthstone Deck Tracker/LogReader/Handlers/BobHandler.cs
@@ -12,6 +12,13 @@ namespace Hearthstone_Deck_Tracker.LogReader.Handlers
 	{
 		public void Handle(string logLine, IHsGameState gameState, IGame game)
 		{
+			var match = HsLogReaderConstants.LegendRankRegex.Match(logLine);
+			if(match.Success)
+			{
+				var rank = int.Parse(match.Groups["rank"].Value);
+				game.MetaData.LegendRank = rank;
+				return;
+			}
 			if(!logLine.Contains("---Register"))
 				return;
 
