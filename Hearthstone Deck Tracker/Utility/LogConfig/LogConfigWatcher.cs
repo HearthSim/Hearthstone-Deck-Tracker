097e0f7d95c07119770957946afbe260e01a84d4
Alexander Zeier <epikz37@gmail.com>
2016-03-12 18:43:45 +0100

    fix crash when log.config directory does not exist

--- Hearthstone Deck Tracker/Utility/LogConfig/LogConfigWatcher.cs
+++ Hearthstone Deck Tracker/Utility/LogConfig/LogConfigWatcher.cs
@@ -1,7 +1,9 @@
 ﻿#region
 
+using System;
 using System.IO;
 using Hearthstone_Deck_Tracker.Utility.Extensions;
+using Hearthstone_Deck_Tracker.Utility.Logging;
 using static Hearthstone_Deck_Tracker.Utility.LogConfig.LogConfigConstants;
 
 #endregion
@@ -10,19 +12,35 @@ namespace Hearthstone_Deck_Tracker.Utility.LogConfig
 {
 	internal class LogConfigWatcher
 	{
-		private static FileSystemWatcher FileWatcher { get; } = new FileSystemWatcher(HearthstoneAppData, LogConfigFile)
-		{
-			EnableRaisingEvents = true
-		};
+		private static FileSystemWatcher _fileWatcher;
 
 		public static void Start()
 		{
-			FileWatcher.Changed += (sender, args) => LogConfigUpdater.Run().Forget();
-			FileWatcher.Deleted += (sender, args) => LogConfigUpdater.Run().Forget();
+			try
+			{
+				_fileWatcher = new FileSystemWatcher(HearthstoneAppData, LogConfigFile)
+				{
+					EnableRaisingEvents = true
+				};
+				_fileWatcher.Changed += (sender, args) => LogConfigUpdater.Run().Forget();
+				_fileWatcher.Deleted += (sender, args) => LogConfigUpdater.Run().Forget();
+			}
+			catch(Exception e)
+			{
+				Log.Error(e);
+			}
 		}
 
-		public static void Pause() => FileWatcher.EnableRaisingEvents = false;
+		public static void Pause()
+		{
+			if(_fileWatcher != null)
+				_fileWatcher.EnableRaisingEvents = false;
+		}
 
-		public static void Continue() => FileWatcher.EnableRaisingEvents = true;
+		public static void Continue()
+		{
+			if(_fileWatcher != null)
+				_fileWatcher.EnableRaisingEvents = true;
+		}
 	}
 }
