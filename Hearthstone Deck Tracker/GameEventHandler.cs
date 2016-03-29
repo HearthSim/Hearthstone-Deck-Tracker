40c7cab5f08a4e56f46a68ba1ec99d12951494bd
Alexander Zeier <epikz37@gmail.com>
2016-03-29 10:36:19 +0200

    fix post-game matching deck check

--- Hearthstone Deck Tracker/GameEventHandler.cs
+++ Hearthstone Deck Tracker/GameEventHandler.cs
@@ -571,7 +571,7 @@ namespace Hearthstone_Deck_Tracker
 			if(selectedDeck != null)
 			{
 				if(Config.Instance.DiscardGameIfIncorrectDeck
-				   && !_game.Player.RevealedCards.Where(x => !x.Info.Created)
+				   && !_game.Player.RevealedCards.Where(x => (x.IsMinion || x.IsSpell || x.IsWeapon) && !x.Info.Created && !x.Info.Stolen)
 				   .GroupBy(x => x.CardId).All(x => selectedDeck.GetSelectedDeckVersion().Cards.Any(c2 => x.Key == c2.Id && x.Count() <= c2.Count)))
 				{
 					if(Config.Instance.AskBeforeDiscardingGame)
