8f3969988636ce9637e153319b82e50122575a9c
Alexander Zeier <epikz37@gmail.com>
2016-04-18 16:25:20 +0200

    hsreplay: implement converting/uploading/displaying the replay on demand

--- Hearthstone Deck Tracker/HsReplay/HsReplayManager.cs
+++ Hearthstone Deck Tracker/HsReplay/HsReplayManager.cs
@@ -1,10 +1,14 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.HsReplay.API;
+using Hearthstone_Deck_Tracker.HsReplay.Converter;
+using Hearthstone_Deck_Tracker.Replay;
 using Hearthstone_Deck_Tracker.Stats;
 using Hearthstone_Deck_Tracker.Utility.Logging;
 using static Hearthstone_Deck_Tracker.HsReplay.Constants;
@@ -32,6 +36,39 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 			}
 		}
 
+		public static async Task<bool> ShowReplay(GameStats game)
+		{
+			if(game == null)
+				return false;
+			if(Config.Instance.ForceLocalReplayViewer)
+			{
+				ReplayReader.LaunchReplayViewer(game.ReplayFile);
+				return true;
+			}
+			var rfm = new ReplayFileManager(game);
+			if(!rfm.HasHsReplayFile)
+				await rfm.ConvertAndStoreHsReplay();
+			if(rfm.HasHsReplayFile && (!game.HsReplay?.Uploaded ?? true))
+			{
+				var result = await HsReplayUploader.UploadXml(rfm.HsReplay);
+				if(result.Success)
+				{
+					game.HsReplay = new HsReplayInfo(result.ReplayId);
+					if(DefaultDeckStats.Instance.DeckStats.Any(x => x.DeckId == game.DeckId))
+						DefaultDeckStats.Save();
+					else
+						DeckStatsList.Save();
+				}
+			}
+			if(game.HsReplay?.Uploaded ?? false)
+				Helper.TryOpenUrl(game.HsReplay?.Url);
+			else if(game.HasReplayFile)
+				ReplayReader.LaunchReplayViewer(game.ReplayFile);
+			else
+				return false;
+			return true;
+		}
+
 		public static async Task<bool> Setup()
 		{
 			try
@@ -49,6 +86,28 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 				Log.Error(e);
 				return false;
 			}
+		}
+
+		public static async Task ShowReplay(string fileName)
+		{
+			if(Config.Instance.ForceLocalReplayViewer)
+			{
+				ReplayReader.LaunchReplayViewer(fileName);
+				return;
+			}
+			var rfm = new ReplayFileManager(fileName);
+			if(!rfm.HasHsReplayFile)
+				await rfm.ConvertAndStoreHsReplay();
+			if(rfm.HasHsReplayFile)
+			{
+				var result = await HsReplayUploader.UploadXml(rfm.HsReplay);
+				if(result.Success)
+					Helper.TryOpenUrl(new HsReplayInfo(result.ReplayId).Url);
+				else
+					ReplayReader.LaunchReplayViewer(fileName);
+			}
+			else
+				ReplayReader.LaunchReplayViewer(fileName);
 		}
 	}
 }
