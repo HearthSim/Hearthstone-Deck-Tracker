a9163d769003256f85ab6f466555e4169791cd62
Alexander Zeier <epikz37@gmail.com>
2016-05-12 13:35:59 +0200

    arena stats averages: fix exception on empty arena runs collection

--- Hearthstone Deck Tracker/Stats/CompiledStats/ArenaStats.cs
+++ Hearthstone Deck Tracker/Stats/CompiledStats/ArenaStats.cs
@@ -34,25 +34,25 @@ namespace Hearthstone_Deck_Tracker.Stats.CompiledStats
 
 		public int PacksCountTotal => GetFilteredRuns().Sum(x => x.PackCount);
 
-		public double PacksCountAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Average(x => x.PackCount), 2);
+		public double PacksCountAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Select(x => x.PackCount).DefaultIfEmpty(0).Average(), 2);
 
 		public int GoldTotal => GetFilteredRuns().Sum(x => x.Gold);
 
-		public double GoldAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Average(x => x.Gold), 2);
+		public double GoldAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Select(x => x.Gold).DefaultIfEmpty(0).Average(), 2);
 
 		public int GoldSpent => GetFilteredRuns().Count(x => x.Deck.ArenaReward.PaymentMethod == ArenaPaymentMethod.Gold) * 150;
 
 		public int DustTotal => GetFilteredRuns().Sum(x => x.Dust);
 
-		public double DustAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Average(x => x.Dust), 2);
+		public double DustAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Select(x => x.Dust).DefaultIfEmpty(0).Average(), 2);
 
 		public int CardCountTotal => GetFilteredRuns().Sum(x => x.CardCount);
 
-		public double CardCountAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Average(x => x.CardCount), 2);
+		public double CardCountAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Select(x => x.CardCount).DefaultIfEmpty(0).Average(), 2);
 
 		public int CardCountGolden => GetFilteredRuns().Sum(x => x.CardCountGolden);
 
-		public double CardCountGoldenAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Average(x => x.CardCountGolden), 2);
+		public double CardCountGoldenAveragePerRun => Math.Round(GetFilteredRuns(requireAnyReward: true).Select(x => x.CardCountGolden).DefaultIfEmpty(0).Average(), 2);
 
 		public ClassStats ClassStatsBest => !ClassStats.Any() ? null : ClassStats.OrderByDescending(x => x.WinRate).First();
 		public ClassStats ClassStatsWorst => !ClassStats.Any() ? null : ClassStats.OrderBy(x => x.WinRate).First();
