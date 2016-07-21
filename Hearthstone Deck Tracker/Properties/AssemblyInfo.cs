4653bebff88a1def06947452af4e9081efd571b1
Alexander Zeier <epikz37@gmail.com>
2016-07-21 17:17:18 +0200

    v0.15.12

--- Hearthstone Deck Tracker/Properties/AssemblyInfo.cs
+++ Hearthstone Deck Tracker/Properties/AssemblyInfo.cs
@@ -56,6 +56,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.15.11.0")]
-[assembly: AssemblyFileVersion("0.15.11.0")]
+[assembly: AssemblyVersion("0.15.12.0")]
+[assembly: AssemblyFileVersion("0.15.12.0")]
 [assembly: NeutralResourcesLanguage("en")]
