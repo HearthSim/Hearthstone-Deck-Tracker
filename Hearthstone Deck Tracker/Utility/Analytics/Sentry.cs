773f44d34acfe935fddf19b5504187ddc5895e70
Alexander Zeier <alex@hearthsim.net>
2016-08-02 10:46:46 +0200

    sentry reports: include list of active plugins and full version number

--- Hearthstone Deck Tracker/Utility/Analytics/Sentry.cs
+++ Hearthstone Deck Tracker/Utility/Analytics/Sentry.cs
@@ -1,6 +1,8 @@
 ﻿#region
 
 using System;
+using System.Linq;
+using Hearthstone_Deck_Tracker.Plugins;
 using Hearthstone_Deck_Tracker.Utility.Extensions;
 using SharpRaven;
 using SharpRaven.Data;
@@ -14,13 +16,16 @@ namespace Hearthstone_Deck_Tracker.Utility.Analytics
 	{
 		static Sentry()
 		{
-			Client.Release = Helper.GetCurrentVersion().ToVersionString();
+			Client.Release = Helper.GetCurrentVersion().ToVersionString(true);
 		}
 
 		private static readonly RavenClient Client = new RavenClient("https://0a6c07cee8d141f0bee6916104a02af4:883b339db7b040158cdfc42287e6a791@app.getsentry.com/80405");
 
 		public static string CaptureException(Exception ex)
 		{
+			var plugins = PluginManager.Instance.Plugins.Where(x => x.IsEnabled).ToList();
+			ex.Data.Add("active-plugins", plugins.Any() ? string.Join(", ", plugins.Select(x => x.NameAndVersion)) : "none");
+
 			var exception = new SentryEvent(ex);
 #if(SQUIRREL)
 			exception.Tags.Add("squirrel", "true");
--- Hearthstone Deck Tracker/Utility/Extensions/VersionExtensions.cs
+++ Hearthstone Deck Tracker/Utility/Extensions/VersionExtensions.cs
@@ -4,6 +4,6 @@ namespace Hearthstone_Deck_Tracker.Utility.Extensions
 {
 	public static class VersionExtensions
 	{
-		public static string ToVersionString(this Version version) => $"{version?.Major}.{version?.Minor}.{version?.Build}";
+		public static string ToVersionString(this Version version, bool includeRef = false) => $"{version?.Major}.{version?.Minor}.{version?.Build}{(includeRef ? "." + version?.Revision : "")}";
 	}
 }
