0ed9b2fba015afc917e753eaac59d202041089c9
Alexander Zeier <alex@hearthsim.net>
2016-08-12 17:00:45 +0200

    validate log before post-game upload

--- Hearthstone Deck Tracker/GameEventHandler.cs
+++ Hearthstone Deck Tracker/GameEventHandler.cs
@@ -12,8 +12,8 @@ using Hearthstone_Deck_Tracker.Enums;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.Hearthstone.Entities;
 using Hearthstone_Deck_Tracker.HearthStats.API;
-using Hearthstone_Deck_Tracker.HsReplay;
 using Hearthstone_Deck_Tracker.HsReplay.API;
+using Hearthstone_Deck_Tracker.HsReplay.Converter;
 using Hearthstone_Deck_Tracker.LogReader;
 using Hearthstone_Deck_Tracker.Replay;
 using Hearthstone_Deck_Tracker.Stats;
@@ -140,7 +140,7 @@ namespace Hearthstone_Deck_Tracker
 					&& _game.CurrentGameStats.ReplayFile == null)
 					_game.CurrentGameStats.ReplayFile = ReplayMaker.SaveToDisk(powerLog);
 
-				if(Config.Instance.HsReplayAutoUpload && UploadCurrentGameMode)
+				if(Config.Instance.HsReplayAutoUpload && UploadCurrentGameMode && LogValidator.Validate(powerLog).Valid)
 					LogUploader.Upload(powerLog.ToArray(), _game.MetaData, _game.CurrentGameStats).Forget();
 			}
 		}
