d77878b4839631db4fef3fe97b8fbb134147a677
Alexander Zeier <alex@hearthsim.net>
2016-08-23 18:27:26 +0200

    add HSReplay-Api to bootstrap script

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,3 +1,4 @@
 nuget restore
 git clone https://github.com/HearthSim/HearthDb HearthDb
 git clone https://github.com/HearthSim/HearthMirror HearthMirror
+git clone https://github.com/HearthSim/HSReplay-API-Client.git HSReplay-Api
