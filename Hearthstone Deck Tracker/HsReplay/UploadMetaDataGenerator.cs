b76f9a170950bc17891e1faf753b25e0be4e51ff
Alexander Zeier <alex@hearthsim.net>
2016-08-23 15:29:45 +0200

    ensure UploadMetaDataGenerator doesn't add empty fields

--- Hearthstone Deck Tracker/HsReplay/UploadMetaDataGenerator.cs
+++ Hearthstone Deck Tracker/HsReplay/UploadMetaDataGenerator.cs
@@ -14,28 +14,45 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 	{
 		public static UploadMetaData Generate(string[] log, GameMetaData gameMetaData, GameStats game)
 		{
+			var metaData = new UploadMetaData();
 			var playerInfo = GetPlayerInfo(log, game);
-			return new UploadMetaData
+			if(playerInfo != null)
 			{
-				ServerIp = gameMetaData?.ServerInfo?.Address,
-				ServerPort = gameMetaData?.ServerInfo?.Port.ToString(),
-				GameHandle = gameMetaData?.ServerInfo?.GameHandle.ToString(),
-				ClientHandle = gameMetaData?.ServerInfo?.ClientHandle.ToString(),
-				Reconnected = gameMetaData?.Reconnected ?? false,
-				Resumable = gameMetaData?.ServerInfo?.Resumable ?? false,
-				SpectatePassword = gameMetaData?.ServerInfo?.SpectatorPassword,
-				AuroraPassword = gameMetaData?.ServerInfo?.AuroraPassword,
-				ServerVersion = gameMetaData?.ServerInfo?.Version,
-				MatchStart = game?.StartTime != DateTime.MinValue ? game?.StartTime.ToString("o") : null,
-				HearthstoneBuild = gameMetaData?.HearthstoneBuild ?? game?.HearthstoneBuild ?? (game != null ? BuildDates.GetByDate(game.StartTime) : null),
-				GameType = game != null ? (int)HearthDbConverter.GetGameType(game.GameMode, game.Format) : (int?)null,
-				SpectatorMode = game?.GameMode == GameMode.Spectator,
-				FriendlyPlayerId = game?.FriendlyPlayerId > 0 ? game.FriendlyPlayerId : (playerInfo?.FriendlyPlayerId > 0 ? playerInfo?.FriendlyPlayerId : null),
-				ScenarioId = game?.ScenarioId ?? gameMetaData?.ServerInfo?.Mission,
-				Format = game?.Format != null ? (int)HearthDbConverter.GetFormatType(game.Format) : (int?)null,
-				Player1 = playerInfo?.Player1,
-				Player2 = playerInfo?.Player2
-			};
+				metaData.Player1 = playerInfo.Player1;
+				metaData.Player2 = playerInfo.Player2;
+			}
+			if(!string.IsNullOrEmpty(gameMetaData?.ServerInfo?.Address))
+				metaData.ServerIp = gameMetaData.ServerInfo.Address;
+			if(gameMetaData?.ServerInfo?.Port > 0)
+				metaData.ServerPort = gameMetaData.ServerInfo.Port.ToString();
+			if(gameMetaData?.ServerInfo?.GameHandle > 0)
+				metaData.GameHandle = gameMetaData.ServerInfo.GameHandle.ToString();
+			if(gameMetaData?.ServerInfo?.ClientHandle > 0)
+				metaData.ClientHandle = gameMetaData.ServerInfo.ClientHandle.ToString();
+			if(!string.IsNullOrEmpty(gameMetaData?.ServerInfo?.SpectatorPassword))
+				metaData.SpectatePassword = gameMetaData.ServerInfo.SpectatorPassword;
+			if(!string.IsNullOrEmpty(gameMetaData?.ServerInfo?.AuroraPassword))
+				metaData.AuroraPassword = gameMetaData.ServerInfo.AuroraPassword;
+			if(!string.IsNullOrEmpty(gameMetaData?.ServerInfo?.Version))
+				metaData.ServerVersion = gameMetaData.ServerInfo.Version;
+			if(game?.StartTime > DateTime.MinValue)
+				metaData.MatchStart = game.StartTime.ToString("o");
+			if(game != null)
+				metaData.GameType = (int)HearthDbConverter.GetGameType(game.GameMode, game.Format);
+			if(game?.Format != null)
+				metaData.Format = (int)HearthDbConverter.GetFormatType(game.Format);
+			metaData.SpectatorMode = game?.GameMode == GameMode.Spectator;
+			metaData.Reconnected = gameMetaData?.Reconnected ?? false;
+			metaData.Resumable = gameMetaData?.ServerInfo?.Resumable ?? false;
+			metaData.FriendlyPlayerId = game?.FriendlyPlayerId > 0 ? game.FriendlyPlayerId : (playerInfo?.FriendlyPlayerId > 0 ? playerInfo?.FriendlyPlayerId : null);
+			var scenarioId = game?.ScenarioId ?? gameMetaData?.ServerInfo?.Mission;
+			if(scenarioId > 0)
+				metaData.ScenarioId = scenarioId;
+			var build = gameMetaData?.HearthstoneBuild ?? game?.HearthstoneBuild ?? (game != null ? BuildDates.GetByDate(game.StartTime) : null);
+			if(build > 0)
+				metaData.HearthstoneBuild = build;
+
+			return metaData;
 		}
 
 		private static PlayerInfo GetPlayerInfo(string[] log, GameStats game)
