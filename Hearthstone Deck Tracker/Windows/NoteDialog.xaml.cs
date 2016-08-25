20d23ffe4a58094baa4ff6f41752562771292aaa
Andrew Burnett <andrew@andburn.info>
2016-08-25 19:38:47 +0100

    Show Opponent deck in the note window
    adds a button to show/hide the opponents deck as a text list

--- Hearthstone Deck Tracker/Windows/NoteDialog.xaml.cs
+++ Hearthstone Deck Tracker/Windows/NoteDialog.xaml.cs
@@ -1,8 +1,14 @@
 ﻿#region
 
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using HearthDb;
 using Hearthstone_Deck_Tracker.Stats;
+using static HearthDb.CardIds;
+using static System.Windows.Visibility;
 
 #endregion
 
@@ -22,6 +28,9 @@ namespace Hearthstone_Deck_Tracker
 			_game = game;
 			CheckBoxEnterToSave.IsChecked = Config.Instance.EnterToSaveNote;
 			TextBoxNote.Text = game.Note;
+			DeckList.ItemsSource = game.OpponentCards
+				.Select(x => new NoteCard(x))
+				.OrderBy(x => x.Cost);
 			Show();
 			Activate();
 			TextBoxNote.Focus();
@@ -38,6 +47,23 @@ namespace Hearthstone_Deck_Tracker
 				DeckStatsList.Save();
 			}
 			Close();
+		}
+
+		private void DeckPanelVisibility(Visibility visibility, int span, string prefix)
+		{
+			DeckListContainer.Visibility = visibility;
+			TextBoxNote.SetValue(Grid.ColumnSpanProperty, span);
+			BtnDeck.Content = $"{prefix} OPP DECK";
+		}
+
+		private void BtnDeck_Click(object sender, RoutedEventArgs e)
+		{
+			if(DeckListContainer.Visibility == Visible)
+				DeckPanelVisibility(Collapsed, 3, "SHOW");
+			else
+				DeckPanelVisibility(Visible, 2, "HIDE");
+
+			TextBoxNote.Focus();
 		}
 
 		private void TextBoxNote_OnPreviewKeyDown(object sender, KeyEventArgs e)
@@ -60,6 +86,37 @@ namespace Hearthstone_Deck_Tracker
 				return;
 			Config.Instance.EnterToSaveNote = false;
 			Config.Save();
+		}
+
+		private class NoteCard
+		{
+			public string Name { get; }
+			public string CountText { get; }
+			public Brush TextColor { get; }
+			public int Cost { get; }
+
+			public NoteCard() : this(null)
+			{
+			}
+
+			public NoteCard(TrackedCard tracked)
+			{
+				Card card = null;
+				if(tracked == null || !Cards.All.ContainsKey(tracked.Id))
+				{
+					card = Cards.All[NonCollectible.Neutral.Noooooooooooo];
+					CountText = "x0";
+					TextColor = Brushes.Red;
+				}					
+				else
+				{
+					card = Cards.All[tracked.Id];
+					CountText = $"x{tracked.Count}";
+					TextColor = tracked.Unconfirmed == 0 ? Brushes.Black : Brushes.Red;
+				}
+				Name = card.Name;				
+				Cost = card.Cost;
+			}
 		}
 	}
 }
