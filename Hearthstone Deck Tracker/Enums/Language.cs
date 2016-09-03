4c58b338c787f91506e9cbe1ad553e4b828a6e61
Alexander Zeier <alex@hearthsim.net>
2016-09-03 17:26:28 +0200

    add localization framework and language option

---
+++ Hearthstone Deck Tracker/Enums/Language.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+
+namespace Hearthstone_Deck_Tracker.Enums
+{
+	public enum Language
+	{
+		[Description("English")]
+		enUS,
+		[Description("German")]
+		deDE
+	}
+}
