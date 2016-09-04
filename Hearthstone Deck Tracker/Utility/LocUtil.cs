0b9267dd66255cc5d5a245c47c9cd395a8cd5ae9
Alexander Zeier <alex@hearthsim.net>
2016-09-04 11:50:48 +0200

    add LocUtil for retrieving localized strings from code

---
+++ Hearthstone Deck Tracker/Utility/LocUtil.cs
@@ -0,0 +1,21 @@
+using System.Globalization;
+using WPFLocalizeExtension.Engine;
+
+namespace Hearthstone_Deck_Tracker.Utility
+{
+	public static class LocUtil
+	{
+		public static void UpdateCultureInfo()
+		{
+			var locStr = Config.Instance.Localization.ToString().Insert(2, "-");
+			LocalizeDictionary.Instance.Culture = CultureInfo.GetCultureInfo(locStr);
+		}
+
+		public static string Get(string key, bool upper = false)
+		{
+			var str = LocalizeDictionary.Instance.GetLocalizedObject("HearthstoneDeckTracker", "Strings", key,
+				LocalizeDictionary.Instance.Culture).ToString();
+			return upper ? str.ToUpper(LocalizeDictionary.Instance.Culture) : str;
+		}
+	}
+}
