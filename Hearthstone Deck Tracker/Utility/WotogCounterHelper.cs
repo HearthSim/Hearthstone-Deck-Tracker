6df760a2a273428f3d60d260cfacb956e0d50a82
Devin <mac@gnomelan.com>
2016-09-05 16:10:23 -0400

    Extended Spell Counter to support Arcane Giant
    Renamed Yogg-Saron Counter to Spell Counter and made it work with the
    arcane giant

--- Hearthstone Deck Tracker/Utility/WotogCounterHelper.cs
+++ Hearthstone Deck Tracker/Utility/WotogCounterHelper.cs
@@ -15,18 +15,23 @@ namespace Hearthstone_Deck_Tracker.Utility
 		public static Entity PlayerCthun => Core.Game.Player.PlayerEntities.FirstOrDefault(x => x.CardId == CardIds.Collectible.Neutral.Cthun && x.Info.OriginalZone != null);
 		public static Entity PlayerCthunProxy => Core.Game.Player.PlayerEntities.FirstOrDefault(x => x.CardId == CardIds.NonCollectible.Neutral.Cthun);
 		public static Entity PlayerYogg => Core.Game.Player.PlayerEntities.FirstOrDefault(x => x.CardId == CardIds.Collectible.Neutral.YoggSaronHopesEnd && x.Info.OriginalZone != null);
+		public static Entity PlayerArcaneGiant => Core.Game.Player.PlayerEntities.FirstOrDefault(x => x.CardId == CardIds.Collectible.Neutral.ArcaneGiant && x.Info.OriginalZone != null);
 		public static Entity OpponentCthun => Core.Game.Opponent.PlayerEntities.FirstOrDefault(x => x.CardId == CardIds.Collectible.Neutral.Cthun);
 		public static Entity OpponentCthunProxy => Core.Game.Opponent.PlayerEntities.FirstOrDefault(x => x.CardId == CardIds.NonCollectible.Neutral.Cthun);
 		public static bool PlayerSeenCthun => Core.Game.PlayerEntity?.HasTag(SEEN_CTHUN) ?? false;
 		public static bool OpponentSeenCthun => Core.Game.OpponentEntity?.HasTag(SEEN_CTHUN) ?? false;
 		public static bool? CthunInDeck => DeckContains(CardIds.Collectible.Neutral.Cthun);
 		public static bool? YoggInDeck => DeckContains(CardIds.Collectible.Neutral.YoggSaronHopesEnd);
+		public static bool? ArcaneGiantInDeck => DeckContains(CardIds.Collectible.Neutral.ArcaneGiant);
 
 		public static bool ShowPlayerCthunCounter => !Core.Game.IsInMenu && (Config.Instance.PlayerCthunCounter == DisplayMode.Always
 					|| Config.Instance.PlayerCthunCounter == DisplayMode.Auto && PlayerSeenCthun);
 
-		public static bool ShowPlayerSpellsCounter => !Core.Game.IsInMenu && (Config.Instance.PlayerSpellsCounter == DisplayMode.Always
-					|| Config.Instance.PlayerSpellsCounter == DisplayMode.Auto && YoggInDeck != false && (PlayerYogg != null || YoggInDeck == true));
+		public static bool ShowPlayerSpellsCounter => !Core.Game.IsInMenu && (
+			Config.Instance.PlayerSpellsCounter == DisplayMode.Always
+				|| (Config.Instance.PlayerSpellsCounter == DisplayMode.Auto && YoggInDeck.HasValue && (PlayerYogg != null || YoggInDeck.Value))
+				|| (Config.Instance.PlayerSpellsCounter == DisplayMode.Auto && ArcaneGiantInDeck.HasValue && (PlayerArcaneGiant != null || ArcaneGiantInDeck.Value))
+			);
 
 		public static bool ShowOpponentCthunCounter => !Core.Game.IsInMenu && (Config.Instance.OpponentCthunCounter == DisplayMode.Always
 					|| Config.Instance.OpponentCthunCounter == DisplayMode.Auto && OpponentSeenCthun);
