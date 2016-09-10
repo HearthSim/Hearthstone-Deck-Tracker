ee339268cadd15529fa326e3e6d1ee73b9dc6b8b
虾哥哥 <jfk_jin@sina.com>
2016-09-11 00:39:05 +0800

    localization of ordered overlay parts

--- Hearthstone Deck Tracker/Controls/ElementSorterItem.xaml.cs
+++ Hearthstone Deck Tracker/Controls/ElementSorterItem.xaml.cs
@@ -21,7 +21,33 @@ namespace Hearthstone_Deck_Tracker
 		public ElementSorterItem(string name, bool isChecked, Action<bool> setConfigValue, bool isPlayerList)
 		{
 			InitializeComponent();
-			CheckBox.Content = name;
+			switch (name)
+			{
+				case "Deck Title":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_DeckTitle");
+					break;
+				case "Cards":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_Cards");
+					break;
+				case "Card Counter":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_CardCount");
+					break;
+				case "Fatigue Counter":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_FatigueCount");
+					break;
+				case "Draw Chances":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_DrawChances");
+					break;
+				case "Wins":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_Wins");
+					break;
+				case "Win Rate":
+					CheckBox.Content = Utility.LocUtil.Get("Options_Overlay_SortedElement_WinRate");
+					break;
+				default:
+					CheckBox.Content = name;
+					break;
+			}
 			ItemName = name;
 			CheckBox.IsChecked = isChecked;
 			_setConfigValue = setConfigValue;
