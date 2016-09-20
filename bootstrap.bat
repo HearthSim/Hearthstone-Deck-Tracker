ecdec79b43c037dea3f7a10aa63b38d75db7cd92
Alexander Zeier <alex@hearthsim.net>
2016-09-20 11:27:48 +0200

    move localization files to HearthSim/HDT-Localization

--- bootstrap.bat
+++ bootstrap.bat
@@ -2,3 +2,5 @@ nuget restore
 git clone https://github.com/HearthSim/HearthDb HearthDb
 git clone https://github.com/HearthSim/HearthMirror HearthMirror
 git clone https://github.com/HearthSim/HSReplay-API-Client.git HSReplay-Api
+git clone https://github.com/HearthSim/HDT-Localization HDT-Localization
+xcopy /Y "HDT-Localization\*.resx" "Hearthstone Deck Tracker\Properties\"
