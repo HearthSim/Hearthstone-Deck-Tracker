HearthSim/Hearthstone-Deck-Tracker

commit a4fe9300693f8c2bfa94c5b4699773e3d3fbe4c5

PluginSettings: store relative file path
plugins would get disabled because of  absolute file path changes due to squirrel updates (e.g. /app-1.0/ => /app-1.1/).

fixes #2849

Hearthstone Deck Tracker/Plugins/PluginManager.cs
@@ -253,7 +253,11 @@ namespace Hearthstone_Deck_Tracker.Plugins
 				var settings = XmlManager<List<PluginSettings>>.Load(PluginSettingsFile);
 				foreach(var setting in settings)
 				{
-					var plugin = Plugins.FirstOrDefault(x => x.FileName == setting.FileName && x.Name == setting.Name);
+					var path = setting.FileName;
+					var uri = new Uri(path);
+					if(uri.IsAbsoluteUri)
+						path = string.Join("", uri.Segments.SkipWhile(x => x != "Plugins/"));
+					var plugin = Plugins.FirstOrDefault(x => x.RelativeFilePath == path && x.Name == setting.Name);
 					if(plugin != null)
 						plugin.IsEnabled = setting.IsEnabled;
 				}

Hearthstone Deck Tracker/Plugins/PluginSettings.cs
@@ -12,7 +12,7 @@ namespace Hearthstone_Deck_Tracker.Plugins
 
 		internal PluginSettings(PluginWrapper p)
 		{
-			FileName = p.FileName;
+			FileName = p.RelativeFilePath;
 			Name = p.Name;
 			IsEnabled = p.IsEnabled;
 		}

Hearthstone Deck Tracker/Plugins/PluginWrapper.cs
@@ -37,6 +37,8 @@ namespace Hearthstone_Deck_Tracker.Plugins
 
 		public string NameAndVersion => Name + " " + (Plugin?.Version.ToString() ?? "");
 
+		public string RelativeFilePath => new Uri(AppDomain.CurrentDomain.BaseDirectory).MakeRelativeUri(new Uri(FileName)).ToString();
+
 		public bool IsEnabled
 		{
 			get { return _isEnabled; }

commit f542d5494e5210fcaee77cd4b7ab6e4dff0909a6

fix LoadPluginSettings crashing due to malformed uri
The relative plugin path introduced in a4fe930 is no valid uri

Hearthstone Deck Tracker/Plugins/PluginManager.cs
@@ -254,9 +254,11 @@ namespace Hearthstone_Deck_Tracker.Plugins
 				foreach(var setting in settings)
 				{
 					var path = setting.FileName;
-					var uri = new Uri(path);
-					if(uri.IsAbsoluteUri)
+					if(Uri.IsWellFormedUriString(path, UriKind.Absolute))
+					{
+						var uri = new Uri(path, UriKind.Absolute);
 						path = string.Join("", uri.Segments.SkipWhile(x => x != "Plugins/"));
+					}
 					var plugin = Plugins.FirstOrDefault(x => x.RelativeFilePath == path && x.Name == setting.Name);
 					if(plugin != null)
 						plugin.IsEnabled = setting.IsEnabled;