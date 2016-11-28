d1c44775269fc5953c373b17c891fa2a5319738f
Alexander Zeier <alex@hearthsim.net>
2016-11-28 12:05:54 +0100

    ResourceGenerator: automatically download and optimize missing tiles

--- ResourceGenerator/Program.cs
+++ ResourceGenerator/Program.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Net;
 using System.Resources;
 using HearthDb;
+using HearthDb.Enums;
+using nQuant;
 
 namespace ResourceGenerator
 {
 	internal class Program
 	{
+		private static WebClient _webClient;
+		private static WebClient WebClient => _webClient ?? (_webClient = new WebClient());
 		private static void Main(string[] args)
 		{
 			var dir = args[0];
@@ -35,11 +41,40 @@ namespace ResourceGenerator
 					foreach(var card in dict[set])
 					{
 						var img = new FileInfo($"{tilesDir}\\{card.Id}.png");
+						if(!img.Exists && card.Collectible && card.Type != CardType.HERO)
+						{
+							DownloadTile(card, img);
+							img = new FileInfo($"{tilesDir}\\{card.Id}.png");
+						}
 						if(!img.Exists)
 							continue;
 						rw.AddResource(card.Id, new Bitmap(img.FullName));
 					}
 				}
+			}
+		}
+
+		private static void DownloadTile(Card card, FileInfo img)
+		{
+			Console.WriteLine($"Downloading missing image data for {card.Name} ({card.Id})");
+			try
+			{
+				var data = WebClient.DownloadData($"https://art.hearthstonejson.com/v1/tiles/{card.Id}.png");
+				Bitmap src;
+				using(var ms = new MemoryStream(data))
+					src = new Bitmap(new Bitmap(ms), 148, 34);
+				var crop = new Rectangle(0, 0, 130, 34);
+				var target = new Bitmap(crop.Width, crop.Height);
+				using(var g = Graphics.FromImage(target))
+					g.DrawImage(src, crop, new Rectangle(src.Width - crop.Width - 4, 0, crop.Width, src.Height), GraphicsUnit.Pixel);
+
+				var quantizer = new WuQuantizer();
+				using(var quantized = quantizer.QuantizeImage(target, 0, 0))
+					quantized.Save(img.FullName, ImageFormat.Png);
+			}
+			catch(WebException ex)
+			{
+				Console.WriteLine("Error! " + ex.Message);
 			}
 		}
 	}
