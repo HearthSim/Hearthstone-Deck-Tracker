820443e6dc62ca843745707e1e82faa46e639b3b
Alexander Zeier <alex@hearthsim.net>
2016-12-06 09:38:33 +0100

    constructed importing: default to ImportAll checkbox state
    fixes #3013

--- Hearthstone Deck Tracker/Controls/DeckImportingControl.xaml.cs
+++ Hearthstone Deck Tracker/Controls/DeckImportingControl.xaml.cs
@@ -86,8 +86,12 @@ namespace Hearthstone_Deck_Tracker.Controls
 		public void SetDecks(List<ImportedDeck> decks)
 		{
 			Decks.Clear();
+			var import = CheckBoxImportAll.IsChecked == true;
 			foreach(var deck in decks)
+			{
+				deck.Import = import;
 				Decks.Add(deck);
+			}
 			_ready = decks.Any();
 			Text = LocUtil.Get(NoDecksFoundText);
 			UpdateContent();
