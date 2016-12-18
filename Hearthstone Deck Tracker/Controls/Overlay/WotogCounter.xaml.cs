e1c50e85d874287f1033a9ae50483399d6f03a7b
UZERE <blntonb@gmail.com>
2016-12-18 21:25:59 +0300

    add Jade Golem counter UI

--- Hearthstone Deck Tracker/Controls/Overlay/WotogCounter.xaml.cs
+++ Hearthstone Deck Tracker/Controls/Overlay/WotogCounter.xaml.cs
@@ -15,7 +15,10 @@ namespace Hearthstone_Deck_Tracker.Controls.Overlay
 		private string _attack = "6";
 		private string _health = "6";
 		private string _spells = "0";
+		private string _jade = "1";
+
 		private WotogCounterStyle _wotogCounterStyle;
+		private WotogCounterStyle _jadeCounterStyle;
 
 
 		public WotogCounter()
@@ -59,6 +62,17 @@ namespace Hearthstone_Deck_Tracker.Controls.Overlay
 			}
 		}
 
+		public string Jade
+		{
+			get { return _jade; }
+			set {
+				if (value == _jade)
+					return;
+				_jade = value;
+				OnPropertyChanged();
+			}
+		}
+
 		public WotogCounterStyle WotogCounterStyle
 		{
 			get { return _wotogCounterStyle; }
@@ -74,10 +88,22 @@ namespace Hearthstone_Deck_Tracker.Controls.Overlay
 			}
 		}
 
+		public WotogCounterStyle JadeCounterStyle
+		{
+			get { return _jadeCounterStyle; }
+			set {
+				if (value == _jadeCounterStyle)
+					return;
+				_jadeCounterStyle = value;
+				OnPropertyChanged(nameof(JadeVisibility));
+			}
+		}
+
 		public int IconWidth => WotogCounterStyle == WotogCounterStyle.Full ? 226 : 145;
 		public Visibility CthunVisibility => _forceShow || WotogCounterStyle == WotogCounterStyle.Cthun ? Visibility.Visible : Visibility.Collapsed;
 		public Visibility SpellsVisibility => !_forceShow && WotogCounterStyle == WotogCounterStyle.Spells ? Visibility.Visible : Visibility.Collapsed;
 		public Visibility FullVisibility => !_forceShow && WotogCounterStyle == WotogCounterStyle.Full ? Visibility.Visible : Visibility.Collapsed;
+		public Visibility JadeVisibility => !_forceShow && JadeCounterStyle == WotogCounterStyle.Full ? Visibility.Visible : Visibility.Collapsed;
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -93,6 +119,7 @@ namespace Hearthstone_Deck_Tracker.Controls.Overlay
 			OnPropertyChanged(nameof(CthunVisibility));
 			OnPropertyChanged(nameof(SpellsVisibility));
 			OnPropertyChanged(nameof(FullVisibility));
+			OnPropertyChanged(nameof(JadeVisibility));
 		}
 	}
 
