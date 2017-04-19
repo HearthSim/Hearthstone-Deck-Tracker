9558fa5fe19cca5f67227a489fc87454442486ce
Nobbler <jonathan.bakert@gmail.com>
2017-04-19 13:55:13 -0400

    Resolved incorrect target path and reverted to nuget restore.  msbuild /t:restore requires packages to be managed differently.

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,7 +1,6 @@
 @echo off
 
-msbuild /t:restore
-REM nuget restore
+nuget restore
 git clone https://github.com/HearthSim/HearthDb HearthDb
 git clone https://github.com/HearthSim/HearthMirror HearthMirror
 git clone https://github.com/HearthSim/HSReplay-API-Client.git HSReplay-Api
