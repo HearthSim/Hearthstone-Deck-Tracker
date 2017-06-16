ea2e007467bb7bbd6d25123b609a9d57b0080104
judge2020 <hunterray55@hotmail.com>
2017-06-16 06:17:11 -0400

    Clean up old export options from config

--- Hearthstone Deck Tracker/Config.cs
+++ Hearthstone Deck Tracker/Config.cs
@@ -293,68 +293,8 @@ namespace Hearthstone_Deck_Tracker
 		[DefaultValue(true)]
 		public bool EnterToSaveNote = true;
 
-		[DefaultValue(0.06)]
-		public double ExportAllButtonX = 0.06;
-
-		[DefaultValue(0.915)]
-		public double ExportAllButtonY = 0.915;
-
-		[DefaultValue(0.118)]
-		public double ExportZeroButtonX = 0.118;
-
-		[DefaultValue(0.917)]
-		public double ExportZeroButtonY = 0.917;
-
-		[DefaultValue(0.108)]
-		public double ExportZeroSquareX = 0.108;
-
-		[DefaultValue(0.907)]
-		public double ExportZeroSquareY = 0.907;
-
-		[DefaultValue(0.049)]
-		public double ExportSetsButtonX = 0.049;
-
-		[DefaultValue(0.917)]
-		public double ExportSetsButtonY = 0.917;
-
-		[DefaultValue(0.067)]
-		public double ExportAllSetsButtonX = 0.067;
-
-		[DefaultValue(0.45)]
-		public double ExportStandardSetButtonY = 0.45;
-
-		[DefaultValue(0.04)]
-		public double ExportCard1X = 0.04;
-
-		[DefaultValue(0.2)]
-		public double ExportCard2X = 0.2;
-
-		[DefaultValue(0.168)]
-		public double ExportCardsY = 0.168;
-
-		[DefaultValue(0.185)]
-		public double ExportClearCheckYFixed = 0.185;
-
-		[DefaultValue(0.83)]
-		public double ExportClearX = 0.83;
-
-		[DefaultValue(0.13)]
-		public double ExportClearY = 0.13;
-
 		[DefaultValue(true)]
 		public bool ExportIncludeVersion = true;
-
-		[DefaultValue(0.85)]
-		public double ExportNameDeckX = 0.85;
-
-		[DefaultValue(0.075)]
-		public double ExportNameDeckY = 0.075;
-
-		[DefaultValue(0.5)]
-		public double ExportSearchBoxX = 0.5;
-
-		[DefaultValue(0.915)]
-		public double ExportSearchBoxY = 0.915;
 
 		[DefaultValue(false)]
 		public bool ExtraFeatures = false;
@@ -1053,7 +993,7 @@ namespace Hearthstone_Deck_Tracker
 #if(SQUIRREL)
 				return AppDataPath + "\\";
 #else
-				return Instance.SaveDataInAppData == false ? DataDirPath + "\\" : AppDataPath + "\\";;
+				return Instance.SaveDataInAppData == false ? DataDirPath + "\\" : AppDataPath + "\\";
 #endif
 			}
 		}
