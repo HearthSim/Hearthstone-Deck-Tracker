8b0666fec8a11f779396464c4abe110c3f986992
Alexander Zeier <alex@hearthsim.net>
2017-07-17 15:24:25 +0200

    Fix deck importing not finding existing decks in versions

--- Hearthstone Deck Tracker/Importing/Game/ImportOptions/ExistingDeck.cs
+++ Hearthstone Deck Tracker/Importing/Game/ImportOptions/ExistingDeck.cs
@@ -21,12 +21,26 @@ namespace Hearthstone_Deck_Tracker.Importing.Game.ImportOptions
 		{
 			Deck = deck;
 			var tmp = new Deck { Cards = new ObservableCollection<Card>(newDeck.Cards.Select(x => new Card { Id = x.Id, Count = x.Count })) };
-			var diff = tmp - deck;
-			MatchingCards = 30 - Math.Max(diff.Where(x => x.Count > 0).Sum(x => x.Count), diff.Where(x => x.Count < 0).Sum(x => -x.Count));
+			MatchingCards = 0;
+			if(deck.HasVersions)
+			{
+				var counts = deck.VersionsIncludingSelf.Select(v => GetMatchingCards(tmp, deck.GetVersion(v)));
+				if(counts.Any(c => c == 30))
+					MatchingCards = 30;
+			}
+			if(MatchingCards != 30)
+				MatchingCards = GetMatchingCards(tmp, deck);
 			NewVersion = MatchingCards == 30 ? new SerializableVersion(0, 0)
 				: (MatchingCards < 26 ? SerializableVersion.IncreaseMajor(deck.Version)
 					: SerializableVersion.IncreaseMinor(deck.Version));
 			ShouldBeNewDeck = MatchingCards < 15;
+		}
+
+		private int GetMatchingCards(Deck deck1, Deck deck2)
+		{
+			var diff = deck1 - deck2;
+			return 30 - Math.Max(diff.Where(x => x.Count > 0).Sum(x => x.Count),
+						diff.Where(x => x.Count < 0).Sum(x => -x.Count));
 		}
 	}
 }
