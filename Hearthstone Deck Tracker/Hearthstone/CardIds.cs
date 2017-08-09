50ef25c110bd3bdc54e23fd25415b12b4648e50e
Alexander Zeier <alex@hearthsim.net>
2017-08-09 10:34:26 +0200

    Clean up CardIds file

--- Hearthstone Deck Tracker/Hearthstone/CardIds.cs
+++ Hearthstone Deck Tracker/Hearthstone/CardIds.cs
@@ -1,8 +1,8 @@
 #region
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using static HearthDb.CardIds;
 
 #endregion
 
@@ -10,85 +10,51 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 {
 	public class CardIds
 	{
-		public static readonly List<string> InvalidCardIds = new List<string>
-		{
-			"EX1_tk34",
-			"EX1_tk29",
-			"EX1_tk28",
-			"EX1_tk11",
-			"EX1_598",
-			"NEW1_032",
-			"NEW1_033",
-			"NEW1_034",
-			"NEW1_009",
-			"CS2_052",
-			"CS2_082",
-			"CS2_051",
-			"CS2_050",
-			"CS2_152",
-			"skele11",
-			"skele21",
-			"GAME",
-			"DREAM",
-			"NEW1_006",
-			"NAX",
-			"FP1_006",
-			"PART",
-			"BRMA",
-			"BRMC",
-			"TBA",
-			"TB_",
-			"TBST_",
-			"LOEA",
-			"LOE_008",
-			"LOE_030"
-		};
-
 		// todo: conditional deathrattle summons: Voidcaller
 		public static readonly Dictionary<string, int> DeathrattleSummonCardIds = new Dictionary<string, int>
 		{
-			{HearthDb.CardIds.Collectible.Druid.MountedRaptor, 1},
-			{HearthDb.CardIds.Collectible.Hunter.SavannahHighmane, 2},
-			{HearthDb.CardIds.Collectible.Rogue.Anubarak, 1},
-			{HearthDb.CardIds.Collectible.Warlock.Dreadsteed, 1},
-			{HearthDb.CardIds.Collectible.Warlock.Voidcaller, 1}, //false negative better than false positive
-			{HearthDb.CardIds.Collectible.Neutral.CairneBloodhoof, 1},
-			{HearthDb.CardIds.Collectible.Neutral.HarvestGolem, 1},
-			{HearthDb.CardIds.Collectible.Neutral.PilotedShredder, 1},
-			{HearthDb.CardIds.Collectible.Neutral.PilotedSkyGolem, 1},
-			{HearthDb.CardIds.Collectible.Neutral.SneedsOldShredder, 1},
-			{HearthDb.CardIds.Collectible.Neutral.HauntedCreeper, 2},
-			{HearthDb.CardIds.Collectible.Neutral.NerubianEgg, 1},
-			{HearthDb.CardIds.Collectible.Neutral.SludgeBelcher, 1},
-			{HearthDb.CardIds.Collectible.Neutral.WobblingRunts, 3}
+			{Collectible.Druid.MountedRaptor, 1},
+			{Collectible.Hunter.SavannahHighmane, 2},
+			{Collectible.Rogue.Anubarak, 1},
+			{Collectible.Warlock.Dreadsteed, 1},
+			{Collectible.Warlock.Voidcaller, 1}, //false negative better than false positive
+			{Collectible.Neutral.CairneBloodhoof, 1},
+			{Collectible.Neutral.HarvestGolem, 1},
+			{Collectible.Neutral.PilotedShredder, 1},
+			{Collectible.Neutral.PilotedSkyGolem, 1},
+			{Collectible.Neutral.SneedsOldShredder, 1},
+			{Collectible.Neutral.HauntedCreeper, 2},
+			{Collectible.Neutral.NerubianEgg, 1},
+			{Collectible.Neutral.SludgeBelcher, 1},
+			{Collectible.Neutral.WobblingRunts, 3}
 		};
 
 		public static readonly Dictionary<string, string> HeroIdDict = new Dictionary<string, string>
 		{
-			{HearthDb.CardIds.Collectible.Warrior.GarroshHellscream, "Warrior"},
-			{HearthDb.CardIds.Collectible.Shaman.Thrall, "Shaman"},
-			{HearthDb.CardIds.Collectible.Rogue.ValeeraSanguinar, "Rogue"},
-			{HearthDb.CardIds.Collectible.Paladin.UtherLightbringer, "Paladin"},
-			{HearthDb.CardIds.Collectible.Hunter.Rexxar, "Hunter"},
-			{HearthDb.CardIds.Collectible.Druid.MalfurionStormrage, "Druid"},
-			{HearthDb.CardIds.Collectible.Warlock.Guldan, "Warlock"},
-			{HearthDb.CardIds.Collectible.Mage.JainaProudmoore, "Mage"},
-			{HearthDb.CardIds.Collectible.Priest.AnduinWrynn, "Priest"},
-			{HearthDb.CardIds.Collectible.Warlock.LordJaraxxus, "Jaraxxus"},
-			{HearthDb.CardIds.Collectible.Neutral.MajordomoExecutus, "Ragnaros the Firelord"}
+			{Collectible.Warrior.GarroshHellscream, "Warrior"},
+			{Collectible.Shaman.Thrall, "Shaman"},
+			{Collectible.Rogue.ValeeraSanguinar, "Rogue"},
+			{Collectible.Paladin.UtherLightbringer, "Paladin"},
+			{Collectible.Hunter.Rexxar, "Hunter"},
+			{Collectible.Druid.MalfurionStormrage, "Druid"},
+			{Collectible.Warlock.Guldan, "Warlock"},
+			{Collectible.Mage.JainaProudmoore, "Mage"},
+			{Collectible.Priest.AnduinWrynn, "Priest"},
+			{Collectible.Warlock.LordJaraxxus, "Jaraxxus"},
+			{Collectible.Neutral.MajordomoExecutus, "Ragnaros the Firelord"}
 		};
 
 		public static readonly Dictionary<string, string> HeroNameDict = new Dictionary<string, string>
 		{
-			{"Warrior", HearthDb.CardIds.Collectible.Warrior.GarroshHellscream},
-			{"Shaman", HearthDb.CardIds.Collectible.Shaman.Thrall},
-			{"Rogue", HearthDb.CardIds.Collectible.Rogue.ValeeraSanguinar},
-			{"Paladin", HearthDb.CardIds.Collectible.Paladin.UtherLightbringer},
-			{"Hunter", HearthDb.CardIds.Collectible.Hunter.Rexxar},
-			{"Druid", HearthDb.CardIds.Collectible.Druid.MalfurionStormrage},
-			{"Warlock", HearthDb.CardIds.Collectible.Warlock.Guldan},
-			{"Mage", HearthDb.CardIds.Collectible.Mage.JainaProudmoore},
-			{"Priest", HearthDb.CardIds.Collectible.Priest.AnduinWrynn}
+			{"Warrior", Collectible.Warrior.GarroshHellscream},
+			{"Shaman", Collectible.Shaman.Thrall},
+			{"Rogue", Collectible.Rogue.ValeeraSanguinar},
+			{"Paladin", Collectible.Paladin.UtherLightbringer},
+			{"Hunter", Collectible.Hunter.Rexxar},
+			{"Druid", Collectible.Druid.MalfurionStormrage},
+			{"Warlock", Collectible.Warlock.Guldan},
+			{"Mage", Collectible.Mage.JainaProudmoore},
+			{"Priest", Collectible.Priest.AnduinWrynn}
 		};
 
 		public static class Secrets
@@ -105,16 +71,16 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 			public static class Hunter
 			{
 				public static List<string> All => new List<string> {BearTrap, CatTrick, DartTrap, ExplosiveTrap, FreezingTrap, HiddenCache, Misdirection, Snipe, SnakeTrap, VenomstrikeTrap};
-				public static string BearTrap => HearthDb.CardIds.Collectible.Hunter.BearTrap;
-				public static string CatTrick => HearthDb.CardIds.Collectible.Hunter.CatTrick;
-				public static string DartTrap => HearthDb.CardIds.Collectible.Hunter.DartTrap;
-				public static string ExplosiveTrap => HearthDb.CardIds.Collectible.Hunter.ExplosiveTrap;
-				public static string FreezingTrap => HearthDb.CardIds.Collectible.Hunter.FreezingTrap;
-				public static string HiddenCache => HearthDb.CardIds.Collectible.Hunter.HiddenCache;
-				public static string Misdirection => HearthDb.CardIds.Collectible.Hunter.Misdirection;
-				public static string Snipe => HearthDb.CardIds.Collectible.Hunter.Snipe;
-				public static string SnakeTrap => HearthDb.CardIds.Collectible.Hunter.SnakeTrap;
-				public static string VenomstrikeTrap => HearthDb.CardIds.Collectible.Hunter.VenomstrikeTrap;
+				public static string BearTrap => Collectible.Hunter.BearTrap;
+				public static string CatTrick => Collectible.Hunter.CatTrick;
+				public static string DartTrap => Collectible.Hunter.DartTrap;
+				public static string ExplosiveTrap => Collectible.Hunter.ExplosiveTrap;
+				public static string FreezingTrap => Collectible.Hunter.FreezingTrap;
+				public static string HiddenCache => Collectible.Hunter.HiddenCache;
+				public static string Misdirection => Collectible.Hunter.Misdirection;
+				public static string Snipe => Collectible.Hunter.Snipe;
+				public static string SnakeTrap => Collectible.Hunter.SnakeTrap;
+				public static string VenomstrikeTrap => Collectible.Hunter.VenomstrikeTrap;
 
 				public static List<string> GetCards(bool standardOnly) => 
 					standardOnly ? All.Where(x => !Helper.WildOnlySets.Contains(Database.GetCardFromId(x).Set)).ToList() : All;
@@ -123,17 +89,17 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 			public static class Mage
 			{
 				public static List<string> All => new List<string> {Counterspell, Duplicate, Effigy, FrozenClone, IceBarrier, IceBlock, ManaBind, MirrorEntity, PotionOfPolymorph, Spellbender, Vaporize};
-				public static string Counterspell => HearthDb.CardIds.Collectible.Mage.Counterspell;
-				public static string Duplicate => HearthDb.CardIds.Collectible.Mage.Duplicate;
-				public static string Effigy => HearthDb.CardIds.Collectible.Mage.Effigy;
-				public static string FrozenClone => HearthDb.CardIds.Collectible.Mage.FrozenClone;
-				public static string IceBarrier => HearthDb.CardIds.Collectible.Mage.IceBarrier;
-				public static string IceBlock => HearthDb.CardIds.Collectible.Mage.IceBlock;
-				public static string ManaBind => HearthDb.CardIds.Collectible.Mage.ManaBind;
-				public static string MirrorEntity => HearthDb.CardIds.Collectible.Mage.MirrorEntity;
-				public static string PotionOfPolymorph => HearthDb.CardIds.Collectible.Mage.PotionOfPolymorph;
-				public static string Spellbender => HearthDb.CardIds.Collectible.Mage.Spellbender;
-				public static string Vaporize => HearthDb.CardIds.Collectible.Mage.Vaporize;
+				public static string Counterspell => Collectible.Mage.Counterspell;
+				public static string Duplicate => Collectible.Mage.Duplicate;
+				public static string Effigy => Collectible.Mage.Effigy;
+				public static string FrozenClone => Collectible.Mage.FrozenClone;
+				public static string IceBarrier => Collectible.Mage.IceBarrier;
+				public static string IceBlock => Collectible.Mage.IceBlock;
+				public static string ManaBind => Collectible.Mage.ManaBind;
+				public static string MirrorEntity => Collectible.Mage.MirrorEntity;
+				public static string PotionOfPolymorph => Collectible.Mage.PotionOfPolymorph;
+				public static string Spellbender => Collectible.Mage.Spellbender;
+				public static string Vaporize => Collectible.Mage.Vaporize;
 
 				public static List<string> GetCards(bool standardOnly) =>
 					standardOnly ? All.Where(x => !Helper.WildOnlySets.Contains(Database.GetCardFromId(x).Set)).ToList() : All;
@@ -142,14 +108,14 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 			public static class Paladin
 			{
 				public static List<string> All => new List<string> {Avenge, CompetitiveSpirit, EyeForAnEye, GetawayKodo, NobleSacrifice, Redemption, Repentance, SacredTrial};
-				public static string Avenge => HearthDb.CardIds.Collectible.Paladin.Avenge;
-				public static string CompetitiveSpirit => HearthDb.CardIds.Collectible.Paladin.CompetitiveSpirit;
-				public static string EyeForAnEye => HearthDb.CardIds.Collectible.Paladin.EyeForAnEye;
-				public static string GetawayKodo => HearthDb.CardIds.Collectible.Paladin.GetawayKodo;
-				public static string NobleSacrifice => HearthDb.CardIds.Collectible.Paladin.NobleSacrifice;
-				public static string Redemption => HearthDb.CardIds.Collectible.Paladin.Redemption;
-				public static string Repentance => HearthDb.CardIds.Collectible.Paladin.Repentance;
-				public static string SacredTrial => HearthDb.CardIds.Collectible.Paladin.SacredTrial;
+				public static string Avenge => Collectible.Paladin.Avenge;
+				public static string CompetitiveSpirit => Collectible.Paladin.CompetitiveSpirit;
+				public static string EyeForAnEye => Collectible.Paladin.EyeForAnEye;
+				public static string GetawayKodo => Collectible.Paladin.GetawayKodo;
+				public static string NobleSacrifice => Collectible.Paladin.NobleSacrifice;
+				public static string Redemption => Collectible.Paladin.Redemption;
+				public static string Repentance => Collectible.Paladin.Repentance;
+				public static string SacredTrial => Collectible.Paladin.SacredTrial;
 
 				public static List<string> GetCards(bool standardOnly) =>
 					standardOnly ? All.Where(x => !Helper.WildOnlySets.Contains(Database.GetCardFromId(x).Set)).ToList() : All;
