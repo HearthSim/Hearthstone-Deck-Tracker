f73923bcf819b48b047399eedd1162bd03c0bec8
Alexander Zeier <alex@hearthsim.net>
2017-08-23 10:53:56 +0200

    Escape values in influx line protocol

--- Hearthstone Deck Tracker/Utility/Analytics/InfluxPoint.cs
+++ Hearthstone Deck Tracker/Utility/Analytics/InfluxPoint.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Hearthstone_Deck_Tracker.Utility.Extensions;
 
 namespace Hearthstone_Deck_Tracker.Utility.Analytics
 {
 	internal class InfluxPoint
 	{
+		private readonly Regex _escape = new Regex("[,= ]");
+
 		public InfluxPoint(string name, Dictionary<string, object> tags, Dictionary<string, object> fields, DateTime utcNow)
 		{
 			Name = name;
@@ -20,9 +23,14 @@ namespace Hearthstone_Deck_Tracker.Utility.Analytics
 		public Dictionary<string, object> Fields { get; }
 		public DateTime Timestamp { get; }
 
+		private string Escape(string str)
+		{
+			return _escape.Replace(str, "\\$&");
+		}
+
 		public string ToLineProtocol()
 		{
-			var tags = string.Join(",", Tags.Select(x => $"{x.Key}={x.Value}"));
+			var tags = string.Join(",", Tags.Select(x => $"{x.Key}={Escape(x.Value.ToString())}"));
 			var fields = string.Join(",", Fields.Select(x => $"{x.Key}={GetValueString(x.Value)}"));
 			return $"{Name}{(tags.Any() ? $",{tags}" : "")} {fields} {Timestamp.ToUnixTime()}";
 		}
@@ -31,7 +39,7 @@ namespace Hearthstone_Deck_Tracker.Utility.Analytics
 		{
 			if(int.TryParse(value.ToString(), out var intValue))
 				return value.ToString();
-			return $"\"{value}\"";
+			return $"\"{Escape(value.ToString())}\"";
 		}
 	}
 }
