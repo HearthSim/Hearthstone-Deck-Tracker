baed4eb70e031d17b82b2edcb21385fb170adfc6
Alexander Zeier <alex@hearthsim.net>
2017-12-11 00:32:18 +0100

    Implement Fal'dorei Strider token tracking
    Fixed #3492

--- Hearthstone Deck Tracker/LogReader/Handlers/PowerHandler.cs
+++ Hearthstone Deck Tracker/LogReader/Handlers/PowerHandler.cs
@@ -310,6 +310,9 @@ namespace Hearthstone_Deck_Tracker.LogReader.Handlers
 							case Collectible.Neutral.TheDarkness:
 								AddKnownCardId(gameState, NonCollectible.Neutral.TheDarkness_DarknessCandleToken, 3);
 								break;
+							case Collectible.Rogue.FaldoreiStrider:
+								AddKnownCardId(gameState, NonCollectible.Rogue.FaldoreiStrider_SpiderAmbushEnchantment, 3);
+								break;
 							default:
 								if(playerEntity.Value != null && playerEntity.Value.GetTag(GameTag.CURRENT_PLAYER) == 1
 									&& !gameState.PlayerUsedHeroPower
