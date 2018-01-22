acd4c585d22a002449d449cb51cdd8a6f806d7c9
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-01-22 14:20:55 +0100

    Implemented PlayerChallengeable property

--- Hearthstone Deck Tracker/LogReader/Handlers/LoadingScreenHandler.cs
+++ Hearthstone Deck Tracker/LogReader/Handlers/LoadingScreenHandler.cs
@@ -75,9 +75,7 @@ namespace Hearthstone_Deck_Tracker.LogReader.Handlers
 				else
 					Watchers.DungeonRunWatcher.Stop();
 
-				if (game.CurrentMode == Mode.HUB || game.CurrentMode == Mode.TOURNAMENT || game.CurrentMode == Mode.ADVENTURE
-					|| game.CurrentMode == Mode.TAVERN_BRAWL || game.CurrentMode == Mode.DRAFT || game.CurrentMode == Mode.PACKOPENING
-					|| game.CurrentMode == Mode.COLLECTIONMANAGER)
+				if (game.PlayerChallengeable)
 					Watchers.FriendlyChallengeWatcher.Run();
 				else
 					Watchers.FriendlyChallengeWatcher.Stop();
