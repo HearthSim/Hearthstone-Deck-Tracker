eebf5c439ad71190f6e517bb78b8875448d1d1b2
Chris-Lennart Junge <j.chris_lennart@ymail.com>
2018-01-24 15:34:16 +0100

    Added friendly challenge dialog watcher

---
+++ HearthWatcher/EventArgs/DialogVisibilityEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace HearthWatcher.EventArgs
+{
+	public class FriendlyChallengeEventArgs : System.EventArgs
+	{
+		public bool DialogVisible { get; set; }
+
+		public FriendlyChallengeEventArgs(bool dialogVisible)
+		{
+			DialogVisible = dialogVisible;
+		}
+	}
+}
---
+++ HearthWatcher/FriendlyChallengeWatcher.cs
@@ -0,0 +1,54 @@
+﻿using HearthWatcher.EventArgs;
+using HearthWatcher.Providers;
+using System;
+using System.Threading.Tasks;
+
+namespace HearthWatcher
+{
+	public class FriendlyChallengeWatcher
+	{
+		public delegate void FriendlyChallengeEventHandler(object sender, FriendlyChallengeEventArgs args);
+
+		private readonly IFriendlyChallengeProvider _challengeProvider;
+		private readonly int _delay;
+		private bool _running;
+		private bool _watch;
+		private bool _previousVisibilityValue;
+
+		public FriendlyChallengeWatcher(IFriendlyChallengeProvider challengeProvider, int delay = 500)
+		{
+			_challengeProvider = challengeProvider ?? throw new ArgumentNullException(nameof(challengeProvider));
+			_delay = delay;
+		}
+
+		public event FriendlyChallengeEventHandler OnFriendlyChallenge;
+
+		public void Run()
+		{
+			_watch = true;
+			if(!_running)
+				CheckForFriendlyChallenge();
+		}
+
+		public void Stop() => _watch = false;
+
+		private async void CheckForFriendlyChallenge()
+		{
+			_running = true;
+			while(_watch)
+			{
+				await Task.Delay(_delay);
+				if(!_watch)
+					break;
+				var dialogVisible = _challengeProvider.DialogVisible;
+				if(dialogVisible != _previousVisibilityValue)
+				{
+					OnFriendlyChallenge?.Invoke(this, new FriendlyChallengeEventArgs(dialogVisible));
+					_previousVisibilityValue = dialogVisible;
+				}
+			}
+			_previousVisibilityValue = false;
+			_running = false;
+		}
+	}
+}
