fe854aa7cb3e4d17652531ba54656cf45562bf90
Alexander Zeier <alex@hearthsim.net>
2018-03-03 17:30:19 -0700

    Allow multiple values in BoolToVisibilityConverters

--- Hearthstone Deck Tracker/Utility/Converters/BoolToVisibilityConverter.cs
+++ Hearthstone Deck Tracker/Utility/Converters/BoolToVisibilityConverter.cs
@@ -1,17 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 
 namespace Hearthstone_Deck_Tracker.Utility.Converters
 {
-	class BoolToVisibilityConverter : IValueConverter
+	internal static class ConverterHelper
 	{
+		public static bool BoolConverter(object[] values, object parameter)
+		{
+			if(values == null)
+				return false;
+
+			bool GetValue(object obj, bool b = false)
+			{
+				var val = obj as bool? ?? false;
+				return b ? !val : val;
+			}
+
+			var parameters = (parameter as IEnumerable<bool> ?? new List<bool>())
+				.Concat(values.Select(x => false));
+
+			return values.Zip(parameters, GetValue).All(x => x);
+		}
+	}
+
+	public class BoolToVisibilityConverter : IValueConverter, IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			return ConverterHelper.BoolConverter(values, parameter) ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if(value as bool? ?? false)
-				return Visibility.Visible;
-			return Visibility.Collapsed;
+			return Convert(new[] { value }, targetType, parameter, culture);
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -20,13 +50,21 @@ namespace Hearthstone_Deck_Tracker.Utility.Converters
 		}
 	}
 
-	class InverseBoolToVisibilityConverter : IValueConverter
+	public class InverseBoolToVisibilityConverter : IValueConverter, IMultiValueConverter
 	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			return ConverterHelper.BoolConverter(values, parameter) ? Visibility.Collapsed : Visibility.Visible;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if(value as bool? ?? false)
-				return Visibility.Collapsed;
-			return Visibility.Visible;
+			return Convert(new[] { value }, targetType, parameter, culture);
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
