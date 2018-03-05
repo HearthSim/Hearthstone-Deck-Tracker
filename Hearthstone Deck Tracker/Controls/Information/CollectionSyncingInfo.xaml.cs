89c8cadd61aa2f1e01f600764318e6484ca79645
Alexander Zeier <alex@hearthsim.net>
2018-03-04 18:31:25 -0700

    Add empty CollectionSyncingInfo panel

---
+++ Hearthstone Deck Tracker/Controls/Information/CollectionSyncingInfo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+using Hearthstone_Deck_Tracker.HsReplay;
+using Hearthstone_Deck_Tracker.Utility;
+using Hearthstone_Deck_Tracker.Utility.Extensions;
+
+namespace Hearthstone_Deck_Tracker.Controls.Information
+{
+	public partial class CollectionSyncingInfo
+	{
+		public CollectionSyncingInfo()
+		{
+			InitializeComponent();
+		}
+
+		public ICommand LoginCommand => new Command(() =>
+		{
+			Core.MainWindow.FlyoutUpdateNotes.IsOpen = false;
+			Helper.OptionsMain.TreeViewItemHSReplayCollection.IsSelected = true;
+			Core.MainWindow.FlyoutOptions.IsOpen = true;
+			HSReplayNetHelper.TryAuthenticate().Forget();
+		});
+
+		public ICommand CloseCommand => new Command(() => Core.MainWindow.FlyoutUpdateNotes.IsOpen = false);
+	}
+}
--- Hearthstone Deck Tracker/FlyoutControls/UpdateNotes.xaml.cs
+++ Hearthstone Deck Tracker/FlyoutControls/UpdateNotes.xaml.cs
@@ -20,17 +20,36 @@ using Microsoft.Win32;
 
 namespace Hearthstone_Deck_Tracker.FlyoutControls
 {
-	/// <summary>
-	/// Interaction logic for UpdateNotes.xaml
-	/// </summary>
 	public partial class UpdateNotes : INotifyPropertyChanged
 	{
+		private bool _animateTransition;
 		private bool _continueToHighlight;
+
+		private string _releaseNotes;
+
+		private bool _showHighlight;
 
 		public UpdateNotes()
 		{
 			InitializeComponent();
 		}
+
+		public string ReleaseNotes => _releaseNotes ?? (_releaseNotes = GetReleaseNotes());
+
+		public bool ShowHighlight
+		{
+			get => _showHighlight;
+			set
+			{
+				if(_showHighlight != value)
+				{
+					_showHighlight = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		public void SetHighlight(Version previousVersion)
 		{
@@ -40,22 +59,32 @@ namespace Hearthstone_Deck_Tracker.FlyoutControls
 			if(previousVersion < new Version(0, 13, 18))
 				infoControl = new CardThemesInfo();
 #if(!SQUIRREL)
-			if(previousVersion < new Version(0, 15, 14) && Config.Instance.SaveConfigInAppData != false && Config.Instance.SaveDataInAppData != false)
+			if(previousVersion < new Version(0, 15, 14) && Config.Instance.SaveConfigInAppData != false
+														&& Config.Instance.SaveDataInAppData != false)
 			{
 				ContentControlHighlight.Content = new SquirrelInfo();
 				ButtonContinue.Visibility = Visibility.Collapsed;
 				_continueToHighlight = true;
+				_animateTransition = true;
 				return;
 			}
 #endif
-			if(previousVersion < new Version(1,2,4))
+			if(previousVersion < new Version(1, 2, 4))
 				infoControl = new HsReplayStatisticsInfo();
 			if(previousVersion < new Version(1, 5, 2) && IsStreamingSoftwareInstalled())
 				infoControl = new TwitchExtensionInfo();
+			if(previousVersion <= new Version(1, 15, 12))
+			{
+				ContentControlHighlight.Content = new CollectionSyncingInfo();
+				ButtonContinue.Visibility = Visibility.Collapsed;
+				_continueToHighlight = true;
+				return;
+			}
+
 			if(infoControl == null)
 				return;
 			ContentControlHighlight.Content = infoControl;
-			TabControl.SelectedIndex = 1;
+			ShowHighlight = true;
 		}
 
 		private bool IsStreamingSoftwareInstalled()
@@ -66,22 +95,26 @@ namespace Hearthstone_Deck_Tracker.FlyoutControls
 		private bool HasRegistryEntry(string key)
 		{
 			using(var obs = Registry.LocalMachine.OpenSubKey(key))
+			{
 				return obs != null;
+			}
 		}
-
-		private string _releaseNotes;
-		public string ReleaseNotes => _releaseNotes ?? (_releaseNotes = GetReleaseNotes());
 
 		private string GetReleaseNotes()
 		{
 			try
 			{
 				string releaseNotes;
-				using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Hearthstone_Deck_Tracker.Resources.CHANGELOG.md"))
+				using(var stream = Assembly.GetExecutingAssembly()
+					.GetManifestResourceStream("Hearthstone_Deck_Tracker.Resources.CHANGELOG.md"))
 				using(var reader = new StreamReader(stream))
+				{
 					releaseNotes = reader.ReadToEnd();
+				}
+
 				releaseNotes = Regex.Replace(releaseNotes, "\n", "\n\n");
-				releaseNotes = Regex.Replace(releaseNotes, "#(\\d+)", "[#$1](https://github.com/HearthSim/Hearthstone-Deck-Tracker/issues/$1)");
+				releaseNotes = Regex.Replace(releaseNotes, "#(\\d+)",
+					"[#$1](https://github.com/HearthSim/Hearthstone-Deck-Tracker/issues/$1)");
 				return releaseNotes;
 			}
 			catch(Exception ex)
@@ -90,8 +123,6 @@ namespace Hearthstone_Deck_Tracker.FlyoutControls
 				return null;
 			}
 		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
 
 		[NotifyPropertyChangedInvocator]
 		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -103,23 +134,30 @@ namespace Hearthstone_Deck_Tracker.FlyoutControls
 		{
 			if(_continueToHighlight)
 			{
-				TabControl.SelectedIndex = 1;
-				Core.MainWindow.FlyoutUpdateNotes.Header = null;
-				Core.MainWindow.FlyoutUpdateNotes.HeaderTemplate = null;
-				Core.MainWindow.FlyoutUpdateNotes.BeginAnimation(HeightProperty,
-					new DoubleAnimation(Core.MainWindow.FlyoutUpdateNotes.ActualHeight, 400, TimeSpan.FromMilliseconds(250)));
+				ShowHighlight = true;
+				Core.MainWindow.FlyoutUpdateNotes.IsModal = true;
+				Core.MainWindow.FlyoutUpdateNotes.TitleVisibility = Visibility.Collapsed;
+				if(_animateTransition)
+				{
+					Core.MainWindow.FlyoutUpdateNotes.BeginAnimation(HeightProperty,
+						new DoubleAnimation(Core.MainWindow.FlyoutUpdateNotes.ActualHeight, 400, TimeSpan.FromMilliseconds(250)));
+				}
 			}
 			else
 				Core.MainWindow.FlyoutUpdateNotes.IsOpen = false;
 		}
 
-		private void ButtonContinue_OnClick(object sender, RoutedEventArgs e) => TabControl.SelectedIndex = 0;
+		private void ButtonContinue_OnClick(object sender, RoutedEventArgs e)
+		{
+			ShowHighlight = false;
+		}
 
 		private void ButtonHSReplaynet_Click(object sender, RoutedEventArgs e)
 		{
 			var url = Helper.BuildHsReplayNetUrl("premium", "updatenotes");
-			if (!Helper.TryOpenUrl(url))
-				Core.MainWindow.ShowMessage("Could not start your browser", "You can find our premium page at https://hsreplay.net/premium/").Forget();
+			if(!Helper.TryOpenUrl(url))
+				Core.MainWindow.ShowMessage("Could not start your browser",
+					"You can find our premium page at https://hsreplay.net/premium/").Forget();
 		}
 	}
 }
