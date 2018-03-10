e844824b264aadcb75b9fc1a05e86973e18e97dd
Alexander Zeier <alex@hearthsim.net>
2018-03-10 13:54:01 -0700

    Improve CollectionSyncingBanner in synced state

--- Hearthstone Deck Tracker/Windows/MainWindowControls/CollectionSyncingBannerView.xaml.cs
+++ Hearthstone Deck Tracker/Windows/MainWindowControls/CollectionSyncingBannerView.xaml.cs
@@ -1,21 +1,45 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using Hearthstone_Deck_Tracker.Annotations;
 using Hearthstone_Deck_Tracker.HsReplay;
+using Hearthstone_Deck_Tracker.Utility;
 
 namespace Hearthstone_Deck_Tracker.Windows.MainWindowControls
 {
 	public partial class CollectionSyncingBannerView : INotifyPropertyChanged
 	{
+		public static readonly DependencyProperty ContainerProperty = DependencyProperty.Register("Container", typeof(Window),
+			typeof(CollectionSyncingBannerView), new FrameworkPropertyMetadata(OnContainerChanged));
+
+		private static void OnContainerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if(!(d is CollectionSyncingBannerView banner))
+				return;
+			if(e.NewValue != null)
+				((Window)e.NewValue).Activated += (s, a) => banner.Update();
+		}
+
 		public CollectionSyncingBannerView()
 		{
 			InitializeComponent();
-			HSReplayNetHelper.CollectionUploaded += () => OnPropertyChanged(nameof(CollectionSynced));
-			HSReplayNetOAuth.LoggedOut += () => OnPropertyChanged(nameof(CollectionSynced));
+			HSReplayNetHelper.CollectionUploaded += Update;
+			HSReplayNetOAuth.LoggedOut += Update;
 		}
 
 		public bool CollectionSynced => HSReplayNetOAuth.IsFullyAuthenticated && Account.Instance.CollectionState.Any();
+
+		public string SyncAge => CollectionSynced
+			? LocUtil.GetAge(Account.Instance.CollectionState.Values.Max(x => x.Date))
+			: string.Empty;
+
+		public Window Container
+		{
+			get => (Window) GetValue(ContainerProperty);
+			set => SetValue(ContainerProperty, value);
+		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -23,6 +47,12 @@ namespace Hearthstone_Deck_Tracker.Windows.MainWindowControls
 		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private void Update()
+		{
+			OnPropertyChanged(nameof(SyncAge));
+			OnPropertyChanged(nameof(CollectionSynced));
 		}
 	}
 }
