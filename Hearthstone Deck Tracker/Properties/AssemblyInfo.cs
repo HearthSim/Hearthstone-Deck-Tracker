191cfd15fee46a837c2043a50a6e4e8987bdbd95
Alexander Zeier <alex@hearthsim.net>
2018-03-12 12:17:16 -0600

    v1.5.13

--- Hearthstone Deck Tracker/Properties/AssemblyInfo.cs
+++ Hearthstone Deck Tracker/Properties/AssemblyInfo.cs
@@ -56,8 +56,8 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.12")]
-[assembly: AssemblyFileVersion("1.5.12")]
+[assembly: AssemblyVersion("1.5.13")]
+[assembly: AssemblyFileVersion("1.5.13")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
