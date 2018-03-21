d2f639f444829e4a60b617a300145a9270a17a86
Alexander Zeier <alex@hearthsim.net>
2018-03-21 15:07:25 +0100

    Retry loading collection on failure

--- Hearthstone Deck Tracker/Hearthstone/CollectionHelper.cs
+++ Hearthstone Deck Tracker/Hearthstone/CollectionHelper.cs
@@ -17,7 +17,7 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 
 		public static async Task<Collection> GetCollection()
 		{
-			var key = GetCurrentKey();
+			var key = await GetCurrentKey();
 			if(key == null)
 				return null;
 			if(!Collections.TryGetValue(key, out var collection))
@@ -31,15 +31,15 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 		public static bool TryGetCollection(out Collection collection)
 		{
 			collection = null;
-			var key = GetCurrentKey();
+			var key = GetCurrentKey(false).Result;
 			if(key == null)
 				return false;
 			return Collections.TryGetValue(key, out collection);
 		}
 
-		public static async Task UpdateCollection() => await UpdateCollection(GetCurrentKey());
+		public static async Task UpdateCollection() => await UpdateCollection(await GetCurrentKey());
 
-		private static async Task<bool> UpdateCollection(Key key)
+		private static async Task<bool> UpdateCollection(Key key, bool retry = true)
 		{
 			if(DateTime.Now - _lastUpdate < TimeSpan.FromSeconds(2) || key == null)
 				return false;
@@ -57,17 +57,31 @@ namespace Hearthstone_Deck_Tracker.Hearthstone
 				Log.Debug("Updated collection!");
 				return true;
 			}
+			if(retry)
+			{
+				Log.Warn("No collection found, retrying...");
+				await Task.Delay(3000);
+				return await UpdateCollection(key, false);
+			}
 			Log.Warn("No collection found");
 			return false;
 		}
 
-		private static Key GetCurrentKey()
+		private static async Task<Key> GetCurrentKey(bool retry = true)
 		{
 			if(!Core.Game.IsRunning)
 				return _lastUsedKey;
 			var user = Reflection.GetAccountId();
 			if(user == null)
+			{
+				if(_lastUsedKey == null && retry)
+				{
+					Log.Debug("User not found, retrying...");
+					await Task.Delay(3000);
+					return await GetCurrentKey(false);
+				}
 				return _lastUsedKey;
+			}
 			_lastUsedKey = new Key(user.Hi, user.Lo);
 			return _lastUsedKey;
 		}
