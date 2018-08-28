b04f47268f540739364fc5f6798f56e46881b3b7
riQQ <riQQ@users.noreply.github.com>
2018-08-28 22:11:18 +0200

    Harden saving of config files
    Fixes #3678

--- Hearthstone Deck Tracker/Utility/XmlManager.cs
+++ Hearthstone Deck Tracker/Utility/XmlManager.cs
@@ -39,19 +39,50 @@ namespace Hearthstone_Deck_Tracker
 		public static void Save(string path, object obj)
 		{
 			Log.Debug("Saving file: " + path);
+			var tempFile = path + ".tmp";
 			try
 			{
-				using(TextWriter writer = new StreamWriter(path))
+				Serialize(tempFile, obj);
+				if (File.Exists(path))
 				{
-					var xml = new XmlSerializer(typeof(T));
-					xml.Serialize(writer, obj);
+					File.Replace(tempFile, path, path + ".bak");
+				}
+				else
+				{
+					File.Move(tempFile, path);
 				}
 				Log.Debug("File saved: " + path);
 			}
 			catch(Exception e)
 			{
 				Log.Error(e);
+				TryDelete(tempFile);
 			}
+		}
+
+		private static void Serialize(string path, object obj)
+		{
+			using (TextWriter writer = new StreamWriter(path))
+			{
+				var xml = new XmlSerializer(typeof(T));
+				xml.Serialize(writer, obj);
+			}
+		}
+
+		private static bool TryDelete(string path)
+		{
+			if (!File.Exists(path))
+				return true;
+
+			try
+			{
+				File.Delete(path);
+			}
+			catch
+			{
+				return false;
+			}
+			return true;
 		}
 	}
 }
