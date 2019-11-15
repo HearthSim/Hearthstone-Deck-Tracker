097012e193f75b0cc9143ab2d6f516f464522480
riQQ <riQQ@users.noreply.github.com>
2019-11-15 11:46:25 +0100

    Set explicit AppUserModelID
    Fixes #3666

---
+++ Hearthstone Deck Tracker/Utility/Shell32.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Hearthstone_Deck_Tracker.Utility
+{
+	internal class Shell32
+	{
+		[DllImport("shell32.dll", SetLastError = true)]
+		public static extern void SetCurrentProcessExplicitAppUserModelID([MarshalAs(UnmanagedType.LPWStr)] string appUserModelId);		
+	}
+}
