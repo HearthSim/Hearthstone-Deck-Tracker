52a97dd7dcd35eebf399ac131ad17b1ca8b7bfb4
Alexander Zeier <alex@hearthsim.net>
2020-03-26 21:51:11 -0700

    Use TryOpenUrl instead of Process.Start directly

--- Hearthstone Deck Tracker/Utility/Helper.cs
+++ Hearthstone Deck Tracker/Utility/Helper.cs
@@ -692,7 +692,7 @@ namespace Hearthstone_Deck_Tracker
 		internal static void OpenBattlegroundsHeroPicker(int[] heroIds)
 		{
 			var encodedIds = HttpUtility.UrlEncode(string.Join(",", heroIds));
-			Process.Start($"{BuildHsReplayNetUrl("battlegrounds/heroes", "bgs_toast")}#heroes={encodedIds}");
+			TryOpenUrl($"{BuildHsReplayNetUrl("battlegrounds/heroes", "bgs_toast")}#heroes={encodedIds}");
 		}
 	}
 }
