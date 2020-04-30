d84ca19a7cce4ea6d52c282f7dc294202cfe642f
Alexander Zeier <alex@hearthsim.net>
2020-04-30 09:11:13 -0700

    Include replay link in BB Sentry event

--- Hearthstone Deck Tracker/Utility/Analytics/Sentry.cs
+++ Hearthstone Deck Tracker/Utility/Analytics/Sentry.cs
@@ -102,6 +102,7 @@ namespace Hearthstone_Deck_Tracker.Utility.Analytics
 			public TestOutput Output { get; set; }
 
 			public List<string> Log { get; set; }
+			public string Replay => $"https://hsreplay.net/replay/{ShortId}#turn={Turn}b";
 		}
 	}
 }
