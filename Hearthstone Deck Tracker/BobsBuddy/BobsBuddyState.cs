a7388007e22fbb03552530e68a82372fd1d21868
Jonathan Segal <jonysegal@gmail.com>
2020-07-28 17:22:11 -0400

    Add TwitchSimulationState

--- Hearthstone Deck Tracker/BobsBuddy/BobsBuddyState.cs
+++ Hearthstone Deck Tracker/BobsBuddy/BobsBuddyState.cs
@@ -6,4 +6,20 @@
 		Combat,
 		Shopping
 	};
+
+	/// <summary>
+	/// Used to pass information to the twitch overlay to decide whether or not to show the relevant stats.
+	/// See the bobs buddy file in https://github.com/HearthSim/twitch-hdt-frontend/tree/master/src/viewer/overlay
+	/// The values in the enums should match completley; ie, changes to one should be reflected in the other.
+	/// </summary>
+	public enum TwitchSimulationState
+	{
+		WaitingForCombat = 1,
+		OpponentSecrets = 2,
+		TooFewSimulations = 3,
+		UpdateRequired = 4,
+		UnknownCards = 5,
+		InCombat = 6,
+		InNonFirstShoppingPhase = 7,
+	}
 }
