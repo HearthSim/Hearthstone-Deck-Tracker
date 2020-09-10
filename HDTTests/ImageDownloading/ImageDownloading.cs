93a3a5b150ff1f998b6430841aac64d5afea6da8
Jonathan Segal <jonysegal@gmail.com>
2020-09-10 15:01:14 -0400

    Add class to download images async

--- HDTTests/ImageDownloading/ImageDownloading.cs
+++ HDTTests/ImageDownloading/ImageDownloading.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Hearthstone_Deck_Tracker.Importing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,10 @@ namespace HDTTests.ImageDownloading
 	public class ImageDownloading
 	{
 		[TestMethod]
-		public void DownloadImage()
+		public async Task DownloadImage()
 		{
-			const string imageurl = "https://art.hearthstonejson.com/v1/256x/SCH_614.jpg";
-			using(WebClient client = new WebClient())
-			{
-				//client.DownloadFile(new Uri(imageurl), @"C:\temp\image35.png");
-				// OR
-				Console.Write("err");
-				client.DownloadFileAsync(new Uri(imageurl), @"C:\temp\imagee35.jpg");
-			}
+			Task download = CardImageImporter.DownloadCardAsync("BGS_004");
+			await download;
 		}
 	}
 }
---
+++ Hearthstone Deck Tracker/Importing/CardImageImporter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearthstone_Deck_Tracker.Importing
+{
+	public static class CardImageImporter
+	{
+		const string imageurl = "https://art.hearthstonejson.com/v1/256x/SCH_614.jpg";
+		const string HearthstoneArtUrl = "https://art.hearthstonejson.com/v1/256x";
+		private static string StoreImagesPath = Config.AppDataPath + "\\Images";
+
+		static CardImageImporter()
+		{
+			CheckForAndAddImageFolder();
+		}
+
+		static WebClient client = new WebClient();
+		public static async Task DownloadCardAsync(string cardId)
+		{
+			var requestUrl = string.Format($"{HearthstoneArtUrl}/{cardId}.jpg");
+			var storageUrl = string.Format($"{StoreImagesPath}\\{cardId}.jpg");
+			using(client)
+			{
+				await client.DownloadFileTaskAsync(new Uri(requestUrl), storageUrl);
+			}
+		}
+
+		private static bool ImageFolderExists() => Directory.Exists(StoreImagesPath);
+
+		private static void CheckForAndAddImageFolder()
+		{
+			if(!ImageFolderExists())
+				Directory.CreateDirectory(StoreImagesPath);
+		}
+
+	}
+}
