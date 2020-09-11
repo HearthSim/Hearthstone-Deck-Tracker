1458a4659601cdafbc11cc10c122f9f59e2584f4
Jonathan Segal <jonysegal@gmail.com>
2020-09-11 11:53:06 -0400

    Add test for multiple images

--- HDTTests/ImageDownloading/ImageDownloading.cs
+++ HDTTests/ImageDownloading/ImageDownloading.cs
@@ -18,5 +18,22 @@ namespace HDTTests.ImageDownloading
 			Task download = CardImageImporter.DownloadCardAsync("BGS_004");
 			await download;
 		}
+
+		[TestMethod]
+		public async Task DownloadMultipleImages()
+		{
+			List<string> cardIds = new List<string>() { "CS2_231", "OG_300", "AT_022" };
+			List<Task> toAwait = new List<Task>();
+			Console.WriteLine("starting donwload at " + DateTime.Now);
+			foreach(var cardId in cardIds)
+			{
+				Console.WriteLine("Fetching imaage with id " + cardId);
+				toAwait.Add(CardImageImporter.DownloadCardAsync(cardId));
+			}
+			Console.WriteLine("starting wait at " + DateTime.Now);
+			Console.WriteLine("finsihed loop at " + DateTime.Now);
+			await Task.WhenAll(toAwait.ToArray());
+			Console.WriteLine("finished all downlloads at " + DateTime.Now);
+		}
 	}
 }
