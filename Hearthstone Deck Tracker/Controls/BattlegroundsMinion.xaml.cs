6879152c2d3f5f73a10d3e334fe7c163da04ce9d
Jonathan Segal <jonysegal@gmail.com>
2020-09-18 16:42:44 -0400

    Update BGminion control

--- Hearthstone Deck Tracker/Controls/BattlegroundsMinion.xaml.cs
+++ Hearthstone Deck Tracker/Controls/BattlegroundsMinion.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using Hearthstone_Deck_Tracker.Importing;
 
 namespace Hearthstone_Deck_Tracker.Controls
 {
@@ -7,84 +12,65 @@ namespace Hearthstone_Deck_Tracker.Controls
 	/// </summary>
 	public partial class BattlegroundsMinion
 	{
-		public static readonly DependencyProperty HasTooltipProperty = DependencyProperty.Register("HasTooltip", typeof(bool), typeof(Card), new PropertyMetadata(Config.Instance.TrackerCardToolTips));
+		public Visibility PoisonousVisibility { get; set; }
 
-		public Visibility PoisonousVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility DivineShieldVisibility { get; set; }
 
-		public Visibility DivineShieldVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility TauntVisibility { get; set; }
 
-		public Visibility TauntVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility PremiumTauntVisibility { get; set; }
 
-		public Visibility PremiumTauntVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
-		public Visibility DeathrattleVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility DeathrattleVisibility { get; set; }
 
-		public Visibility LegendaryBorderVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility LegendaryBorderVisibility { get; set; }
 
-		public Visibility PremiumLegendaryBorderVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility PremiumLegendaryBorderVisibility { get; set; }
 
-		public Visibility PremiumBorderVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility PremiumBorderVisibility { get; set; }
 
-		public Visibility BorderVisibility
-		{
-			get
-			{
-				return Visibility.Hidden;
-			}
-		}
+		public Visibility BorderVisibility { get; set; }
 
-		public BattlegroundsMinion()
+		public string CardImagePath { get; set; }
+
+		private Entity _entity;
+
+		public BattlegroundsMinion(Entity entity)
 		{
+			_entity = entity;
+			SetEffectVisibilites();
+			CardImagePath = CardImageImporter.StoragePathFor(entity.CardId);
 			InitializeComponent();
 		}
 
-		
+		private void SetEffectVisibilites()
+		{
+			PoisonousVisibility = _entity.HasTag(GameTag.POISONOUS) ? Visibility.Visible : Visibility.Hidden;
+			DivineShieldVisibility = _entity.HasTag(GameTag.DIVINE_SHIELD) ? Visibility.Visible : Visibility.Hidden;
+			DeathrattleVisibility = _entity.HasTag(GameTag.DEATHRATTLE) ? Visibility.Visible : Visibility.Hidden;
+
+			if(_entity.HasTag(GameTag.PREMIUM))
+			{
+				PremiumLegendaryBorderVisibility = _entity.GetTag(GameTag.RARITY) == (int)Rarity.LEGENDARY ? Visibility.Visible : Visibility.Hidden;
+				PremiumBorderVisibility = Visibility.Visible;
+				BorderVisibility = Visibility.Hidden;
+				LegendaryBorderVisibility = Visibility.Hidden;
+				TauntVisibility = Visibility.Hidden;
+				PremiumTauntVisibility = _entity.HasTag(GameTag.TAUNT) ? Visibility.Visible : Visibility.Hidden;
+			}
+			else
+			{
+				PremiumLegendaryBorderVisibility = Visibility.Hidden;
+				PremiumBorderVisibility = Visibility.Hidden;
+				BorderVisibility = Visibility.Visible;
+				LegendaryBorderVisibility = _entity.GetTag(GameTag.RARITY) == (int)Rarity.LEGENDARY ? Visibility.Visible : Visibility.Hidden;
+				TauntVisibility = _entity.HasTag(GameTag.TAUNT) ? Visibility.Visible : Visibility.Hidden;
+				PremiumTauntVisibility = Visibility.Hidden;
+			}
+
+
+
+		}
+
+		private void SetVisibility(Visibility setFor, bool makeVisibile) => setFor = makeVisibile ? Visibility.Visible : Visibility.Hidden;
 	}
 }
