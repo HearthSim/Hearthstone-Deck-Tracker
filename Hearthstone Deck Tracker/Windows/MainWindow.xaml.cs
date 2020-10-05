2a48778a2b646e0833ecc29aa9147613666e30da
Benedict Etzel <benedict@hearthsim.net>
2020-10-05 13:17:07 +0200

    Use dynamic Discord invite URL
    As this may change in future we're using HSReplay.net to forward the user to the final Discord invite URL.

--- Hearthstone Deck Tracker/Windows/MainWindow.xaml.cs
+++ Hearthstone Deck Tracker/Windows/MainWindow.xaml.cs
@@ -372,7 +372,7 @@ namespace Hearthstone_Deck_Tracker.Windows
 
 		private void BtnOptions_OnClick(object sender, RoutedEventArgs e) => FlyoutOptions.IsOpen = true;
 		private void BtnHelp_OnClick(object sender, RoutedEventArgs e) => FlyoutHelp.IsOpen = true;
-		private void BtnDiscord_OnClick(object sender, RoutedEventArgs e) => Helper.TryOpenUrl("https://discord.gg/hearthsim");
+		private void BtnDiscord_OnClick(object sender, RoutedEventArgs e) => Helper.TryOpenUrl("https://hsreplay.net/discord/");
 		private void BtnTwitter_OnClick(object sender, RoutedEventArgs e) => Helper.TryOpenUrl("https://twitter.com/hsreplaynet");
 
 #endregion
