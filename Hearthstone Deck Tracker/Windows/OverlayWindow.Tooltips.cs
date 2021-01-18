5cfdc9b4418344f483aa6b6d021e4fbacb82d2d9
Jonathan Segal <jonysegal@gmail.com>
2021-01-18 03:18:21 -0500

    Add and position images for opponent

--- Hearthstone Deck Tracker/Windows/OverlayWindow.Tooltips.cs
+++ Hearthstone Deck Tracker/Windows/OverlayWindow.Tooltips.cs
@@ -94,13 +94,13 @@ namespace Hearthstone_Deck_Tracker.Windows
 				var centeredListOffset = Config.Instance.OverlayCenterOpponentStackPanel ? (BorderStackPanelOpponent.ActualHeight - StackPanelOpponent.ActualHeight) / 2 : 0;
 				//offset is affected by scaling
 				var topOffset = Canvas.GetTop(BorderStackPanelOpponent) + centeredListOffset
-								+ GetListViewOffset(StackPanelOpponent) + cardIndex * cardSize * Config.Instance.OverlayOpponentScaling / 100;
+								+ GetListViewOffset(StackPanelOpponent) + cardIndex * cardSize * Config.Instance.OverlayOpponentScaling / 100 - ToolTipCardBlock.ActualHeight / 2;
 				var card = ListViewOpponent.Items.Cast<AnimatedCard>().ElementAt(cardIndex).Card;
 				ToolTipCardBlock.SetCardId(card.Id);
 				//prevent tooltip from going outside of the overlay
 				if(topOffset + ToolTipCardBlock.ActualHeight > Height)
 					topOffset = Height - ToolTipCardBlock.ActualHeight;
-
+				topOffset = Math.Max(0, topOffset);
 				SetTooltipPosition(topOffset, BorderStackPanelOpponent);
 
 				ToolTipCardBlock.Visibility = visibility;
