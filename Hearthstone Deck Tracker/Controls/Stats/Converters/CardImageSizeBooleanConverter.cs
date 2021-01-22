7884490fe345fdf7a642445e7832190ae5a39a3d
Jonathan Segal <jonysegal@gmail.com>
2021-01-22 11:46:20 -0500

    Disable card resolution downgrade beyond 1x scale

---
+++ Hearthstone Deck Tracker/Controls/Stats/Converters/CardImageSizeBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Hearthstone_Deck_Tracker.Controls.Stats.Converters
+{
+	public class CardImageSizeBooleanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(targetType != typeof(bool))
+				return null;
+			return (double)value <= 1;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
