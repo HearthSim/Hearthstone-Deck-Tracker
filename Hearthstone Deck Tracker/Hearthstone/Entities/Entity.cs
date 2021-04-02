366b56509acb0938996d05e3f177b29730232686
Alexander Zeier <alex@hearthsim.net>
2021-04-01 17:13:06 -0700

    Fix Entity.IsPlayableHero
    #4257

--- Hearthstone Deck Tracker/Hearthstone/Entities/Entity.cs
+++ Hearthstone Deck Tracker/Hearthstone/Entities/Entity.cs
@@ -62,8 +62,7 @@ namespace Hearthstone_Deck_Tracker.Hearthstone.Entities
 		public bool IsHero => GetTag(GameTag.CARDTYPE) == (int)CardType.HERO;
 
 		[JsonIgnore]
-		// TODO: Replace CardSet check once the ICC update goes live
-		public bool IsPlayableHero => IsHero && Card.CardSet != CardSet.CORE && Card.CardSet != CardSet.HERO_SKINS && Card.Collectible;
+		public bool IsPlayableHero => IsHero && Card.CardSet != CardSet.HERO_SKINS && Card.Collectible;
 
 		[JsonIgnore]
 		public bool IsActiveDeathrattle => HasTag(GameTag.DEATHRATTLE) && GetTag(GameTag.DEATHRATTLE) == 1;
