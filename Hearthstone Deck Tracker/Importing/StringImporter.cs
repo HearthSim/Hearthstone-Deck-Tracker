b5663270f891d0ccbd27d1a185a9caf7df32e34b
SGKoishi <i@sgkoi.dev>
2021-04-27 12:50:10 -0700

    Fix StringImporter ranked cards error
    Fix #4272
    
    Co-authored-by:  Urkaz <mrurkaz@gmail.com>

--- Hearthstone Deck Tracker/Importing/StringImporter.cs
+++ Hearthstone Deck Tracker/Importing/StringImporter.cs
@@ -8,8 +8,8 @@ namespace Hearthstone_Deck_Tracker.Importing
 {
 	public static class StringImporter
 	{
-		private static readonly Regex CardLineRegexCountFirst = new Regex(@"(^(\s*)(?<count>\d)(\s*x)?\s+)(?<cardname>[\w\s'\.:!\-,]+)");
-		private static readonly Regex CardLineRegexCountLast = new Regex(@"(?<cardname>[\w\s'\.:!\-,]+?)(\s+(x\s*)?(?<count>\d))(\s*)$");
+		private static readonly Regex CardLineRegexCountFirst = new Regex(@"(^(\s*)(?<count>\d)(\s*x)?\s+)(?<cardname>[\w\s'\.:!\-(),]+)");
+		private static readonly Regex CardLineRegexCountLast = new Regex(@"(?<cardname>[\w\s'\.:!\-(),]+?)(\s+(x\s*)?(?<count>\d))(\s*)$");
 		public static char[] Separators = { '\n', '|' };
 
 		public static bool IsValidImportString(string importString)
