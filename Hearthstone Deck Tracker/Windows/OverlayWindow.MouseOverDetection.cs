cf1fe3e29460eefe2fca5f0735834fd19cda6508
Édipo da Silva Souza <edipo2s@gmail.com>
2022-06-13 19:28:35 -0300

    Improve final tooltip hover from top

--- Hearthstone Deck Tracker/Windows/OverlayWindow.MouseOverDetection.cs
+++ Hearthstone Deck Tracker/Windows/OverlayWindow.MouseOverDetection.cs
@@ -574,7 +574,7 @@ namespace Hearthstone_Deck_Tracker.Windows
 			// Only BgTierIcons are marked as clickable but a wrapper is scaled by the OverlayElementBehavior
 			if(element == BattlegroundsMinionsPanel.BgTierIcons)
 				return BgsTopBar.RenderTransform as ScaleTransform;
-			if(element == BattlegroundsSession.BattlegroundsSessionPanelTopGroup || element.Name == "BattlegroundsGameViewCanvas")
+			if(element == BattlegroundsSession.BattlegroundsSessionPanelTopGroup || element.Name == "BattlegroundsGameViewContent")
 				return BattlegroundsSession.RenderTransform as ScaleTransform;
 
 			return element.RenderTransform as ScaleTransform;
