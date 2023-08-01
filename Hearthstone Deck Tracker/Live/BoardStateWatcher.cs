9788eb9bab8d4ee4fe2a6438972ad5df892723e3
Benedict Etzel <benedict@hearthsim.net>
2023-08-01 10:59:39 +0200

    fix: only show Buddies on Twitch when enabled

--- Hearthstone Deck Tracker/Live/BoardStateWatcher.cs
+++ Hearthstone Deck Tracker/Live/BoardStateWatcher.cs
@@ -114,6 +114,8 @@ namespace Hearthstone_Deck_Tracker.Live
 
 		private int? BuddyDbfId(Player player)
 		{
+			if(Core.Game.GameEntity?.GetTag(GameTag.BACON_BUDDY_ENABLED) == 0)
+				return null;
 			var hero = player.Board.FirstOrDefault(x => x.IsHero);
 			var buddyDbfId = hero?.GetTag(GameTag.BACON_COMPANION_ID);
 			if(buddyDbfId == 0)
