72e2097782cd012eaa18441a542a0b5eb6375211
Benedict Etzel <benedict@hearthsim.net>
2024-05-23 23:12:19 +0200

    feat: avoid re-allocating LogLine regex

--- HearthWatcher/LogReader/LogLine.cs
+++ HearthWatcher/LogReader/LogLine.cs
@@ -5,33 +5,36 @@ using System.Text.RegularExpressions;
 
 #endregion
 
-namespace HearthWatcher.LogReader
-{
-	public class LogLine
-	{
-		public LogLine(string ns, string line)
-		{
-			Namespace = ns;
-			Line = line;
-			var regex = new Regex("^(D|W) (?<ts>([\\d:.]+)) (?<line>(.*))$");
-			var match = regex.Match(line);
-			if(match.Success)
-			{
-				DateTime time;
-				var ts = match.Groups["ts"].Value;
-				if(DateTime.TryParse(ts, out time))
-				{
-					Time = DateTime.Today.Add(time.TimeOfDay);
-					if(Time > DateTime.Now)
-						Time = Time.AddDays(-1);
-				}
-				LineContent = match.Groups["line"].Value;
-			}
-		}
+namespace HearthWatcher.LogReader;
 
-		public string Namespace { get; set; }
-		public DateTime Time { get; } = DateTime.Now;
-		public string Line { get; set; }
-		public string LineContent { get; set; }
+public class LogLine
+{
+	private static readonly Regex _regex = new(
+		"^(D|W) (?<ts>([\\d:.]+)) (?<line>(.*))$",
+		options: RegexOptions.Compiled
+	);
+
+	public LogLine(string ns, string line)
+	{
+		Namespace = ns;
+		Line = line;
+		var match = _regex.Match(line);
+		if(match.Success)
+		{
+			DateTime time;
+			var ts = match.Groups["ts"].Value;
+			if(DateTime.TryParse(ts, out time))
+			{
+				Time = DateTime.Today.Add(time.TimeOfDay);
+				if(Time > DateTime.Now)
+					Time = Time.AddDays(-1);
+			}
+			LineContent = match.Groups["line"].Value;
+		}
 	}
+
+	public string Namespace { get; }
+	public DateTime Time { get; } = DateTime.Now;
+	public string Line { get; }
+	public string LineContent { get; }
 }
