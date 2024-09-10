b64310d10fcef08b7af28c132c61257494b79e76
Benedict Etzel <benedict@hearthsim.net>
2024-09-10 19:29:37 +0200

    feat: implement Punch Card

---
+++ Hearthstone Deck Tracker/Hearthstone/EffectSystem/Effects/Warrior/PunchCardEnchantment.cs
@@ -0,0 +1,16 @@
+﻿using Hearthstone_Deck_Tracker.Hearthstone.EffectSystem.Enums;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.EffectSystem.Effects.Warrior;
+
+public class PunchCardEnchantment : EntityBasedEffect
+{
+	public override string CardId => HearthDb.CardIds.NonCollectible.Warrior.PunchCard_PunchedInEnchantment;
+	protected override string CardIdToShowInUI => HearthDb.CardIds.Collectible.Warrior.PunchCard;
+
+	public PunchCardEnchantment(int entityId, bool isControlledByPlayer) : base(entityId, isControlledByPlayer)
+	{
+	}
+
+	public override EffectDuration EffectDuration => EffectDuration.SameTurn;
+	public override EffectTag EffectTag => EffectTag.HeroModification;
+}
