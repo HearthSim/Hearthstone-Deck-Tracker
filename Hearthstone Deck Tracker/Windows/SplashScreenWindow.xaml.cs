6c5429538e78a9095501dbeae6692d43e05e4a50
Alex Zeier <alex@hearthsim.net>
2024-11-13 10:06:02 -0800

    feat: remove manual splash screen skipping
    This was always kinda weird anyway

--- Hearthstone Deck Tracker/Windows/SplashScreenWindow.xaml.cs
+++ Hearthstone Deck Tracker/Windows/SplashScreenWindow.xaml.cs
@@ -20,8 +20,6 @@ namespace Hearthstone_Deck_Tracker.Windows
 		private readonly string _installing = LocUtil.Get(LocInstalling);
 		private string _loadingString = LocUtil.Get(LocLoading);
 		private string _versionString = Helper.GetCurrentVersion().ToVersionString();
-		private Visibility _skipVisibility = Visibility.Collapsed;
-		private SolidColorBrush _skipBackground = new SolidColorBrush(Colors.White);
 
 		public SplashScreenWindow()
 		{
@@ -44,30 +42,6 @@ namespace Hearthstone_Deck_Tracker.Windows
 			set
 			{
 				_loadingString = value;
-				OnPropertyChanged();
-			}
-		}
-
-		public Visibility SkipVisibility
-		{
-			get { return _skipVisibility; }
-			set
-			{
-				if(value == _skipVisibility)
-					return;
-				_skipVisibility = value;
-				OnPropertyChanged();
-			}
-		}
-
-		public SolidColorBrush SkipBackground
-		{
-			get { return _skipBackground; }
-			set
-			{
-				if(Equals(value, _skipBackground))
-					return;
-				_skipBackground = value;
 				OnPropertyChanged();
 			}
 		}
@@ -106,23 +80,5 @@ namespace Hearthstone_Deck_Tracker.Windows
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
-
-		private void SkipBorder_OnMouseDown(object sender, MouseButtonEventArgs e)
-		{
-			Log.Info("Skip was pressed");
-			SkipUpdate = true;
-		}
-
-		public async void StartSkipTimer()
-		{
-			await Task.Delay(10000);
-			if(IsVisible)
-				Log.Info("Showing skip button");
-			SkipVisibility = Visibility.Visible;
-		}
-
-		private void SkipBorder_OnMouseEnter(object sender, MouseEventArgs e) => SkipBackground = new SolidColorBrush(Colors.LightBlue);
-
-		private void SkipBorder_OnMouseLeave(object sender, MouseEventArgs e) => SkipBackground = new SolidColorBrush(Colors.White);
 	}
 }
