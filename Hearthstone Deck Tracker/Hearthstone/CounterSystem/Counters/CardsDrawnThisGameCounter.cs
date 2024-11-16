327535b470e8439d8766d8b0fe1c8ed10fcf75e6
mateuscechetto <mateuscechetto@gmail.com>
2024-11-16 01:06:18 -0300

    feat: add Playhouse Giant counter

---
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/Counters/CardsDrawnThisGameCounter.cs
@@ -0,0 +1,61 @@
+﻿using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.Counters;
+
+public class CardsDrawnThisGameCounter: NumericCounter
+{
+	protected override string? CardIdToShowInUI => HearthDb.CardIds.Collectible.Neutral.PlayhouseGiant;
+
+	public override string[] RelatedCards => new string[]
+	{
+		HearthDb.CardIds.Collectible.Neutral.PlayhouseGiant,
+	};
+
+	public CardsDrawnThisGameCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	{
+	}
+
+	public override bool ShouldShow()
+	{
+		if(!Game.IsTraditionalHearthstoneMatch) return false;
+		if(IsPlayerCounter)
+			return InPlayerDeckOrKnown(RelatedCards);
+		return Game.Opponent.OriginalClass?.ToUpperInvariant() == "ROGUE" &&
+			CardUtils.IsCardFromFormat(Database.GetCardFromId(HearthDb.CardIds.Collectible.Neutral.PlayhouseGiant), Game.CurrentFormat) &&
+			Counter >= 10;
+	}
+
+	public override string[] GetCardsToDisplay()
+	{
+		return IsPlayerCounter ?
+			GetCardsInDeckOrKnown(RelatedCards).ToArray() :
+			FilterCardsByClassAndFormat(RelatedCards, Game.Opponent.OriginalClass);
+	}
+
+	public override string ValueToShow() => Counter.ToString();
+	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
+	{
+		if(!Game.IsTraditionalHearthstoneMatch)
+			return;
+
+		if(!Game.IsMulliganDone)
+			return;
+
+		if(entity.IsControlledBy(Game.Player.Id) != IsPlayerCounter)
+			return;
+
+		if(tag != GameTag.ZONE)
+			return;
+
+		if(prevValue != (int)Zone.DECK)
+			return;
+
+		if(value != (int)Zone.HAND)
+			return;
+
+		Counter++;
+
+	}
+}
