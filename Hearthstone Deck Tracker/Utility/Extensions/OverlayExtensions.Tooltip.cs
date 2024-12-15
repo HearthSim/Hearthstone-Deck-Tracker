55a30d35cbbb953a2643e65a4497d6a018086689
Alex Zeier <alex@hearthsim.net>
2024-12-15 10:02:29 -0800

    feat: fall back to default tooltips outside overlay

--- Hearthstone Deck Tracker/Utility/Extensions/OverlayExtensions.Tooltip.cs
+++ Hearthstone Deck Tracker/Utility/Extensions/OverlayExtensions.Tooltip.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using Hearthstone_Deck_Tracker.Windows;
@@ -50,6 +51,23 @@ partial class OverlayExtensions
 			OnToolTipChanged?.Invoke(null, d);
 	}
 
+	private static void OnElementLoaded(object sender, RoutedEventArgs e)
+	{
+		if(sender is not FrameworkElement element)
+			return;
+		if(IsInOverlay(element) || element.ToolTip is not null)
+			return;
+		// For anything not in the overlay, if we don't already have a normal ToolTip, set this as the ToolTip!
+		ToolTipService.SetInitialShowDelay(element, 300);
+		ToolTipService.SetShowDuration(element, 60_000);
+		element.ToolTip = new ToolTip
+		{
+			Content = GetToolTip(element),
+			Background = Brushes.Transparent,
+			BorderBrush = Brushes.Transparent
+		};
+	}
+
 	private static void OnToolTipChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
 	{
 		if(d is not IInputElement inputElement)
@@ -62,11 +80,30 @@ partial class OverlayExtensions
 		{
 			inputElement.MouseEnter += ShowTooltip;
 			inputElement.MouseLeave += HideTooltip;
+
+			if(d is FrameworkContentElement fce)
+				fce.Loaded += OnElementLoaded;
+			else
+			{
+				var fe = d as FrameworkElement ?? Helper.GetLogicalParent<FrameworkElement>(d);
+				if(fe != null)
+					fe.Loaded += OnElementLoaded;
+			}
 		}
 		else
 		{
 			inputElement.MouseEnter -= ShowTooltip;
 			inputElement.MouseLeave -= HideTooltip;
+
+			if(d is FrameworkContentElement fce)
+				fce.Loaded -= OnElementLoaded;
+			else
+			{
+				var fe = d as FrameworkElement ?? Helper.GetLogicalParent<FrameworkElement>(d);
+				if(fe != null)
+					fe.Loaded -= OnElementLoaded;
+			}
+
 			OnToolTipChanged?.Invoke(null, d);
 			_elementIsInOverlay.Remove(d);
 		}
