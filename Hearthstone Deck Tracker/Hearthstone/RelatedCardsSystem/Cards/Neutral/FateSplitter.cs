f1c0eb631bf1eb98e6d0e8eccc189a594a21c93c
mateuscechetto <mateuscechetto@gmail.com>
2025-01-13 19:00:43 -0300

    feat: add Fate Splitter related cards

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Neutral/FateSplitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Neutral;
+
+public class FateSplitter: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Neutral.FateSplitter;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player)
+	{
+
+		var opponent = Core.Game.Player.Id == player.Id ? Core.Game.Opponent : Core.Game.Player;
+
+		var lastCard = opponent.CardsPlayedThisMatch
+			.Select(entity => CardUtils.GetProcessedCardFromEntity(entity, opponent))
+			.LastOrDefault(card => card != null);
+
+		return lastCard != null ? new List<Card?> { lastCard } : new List<Card?>();
+	}
+}
