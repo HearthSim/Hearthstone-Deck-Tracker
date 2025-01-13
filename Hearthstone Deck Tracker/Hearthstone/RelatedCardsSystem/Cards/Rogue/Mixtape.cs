0a9b0adc9a9e436c9311ea3e1d089acb8c7b74fb
mateuscechetto <mateuscechetto@gmail.com>
2025-01-13 18:53:48 -0300

    feat: add Mixtape related cards

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Rogue/Mixtape.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Rogue;
+
+public class Mixtape: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Rogue.Mixtape;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player)
+	{
+
+		var opponent = Core.Game.Player.Id == player.Id ? Core.Game.Opponent : Core.Game.Player;
+
+		return opponent.CardsPlayedThisMatch
+			.Select(entity => CardUtils.GetProcessedCardFromEntity(entity, opponent))
+			.Where(card => card != null)
+			.Distinct()
+			.ToList();
+	}
+}
