0f517266ff0363866adb9ac47d407f69f96b7276
mateuscechetto <mateuscechetto@gmail.com>
2025-01-13 18:39:12 -0300

    feat: add Joymancer Jepetto related cards

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Neutral/JoymancerJepetto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Neutral;
+
+public class JoymancerJepetto: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Neutral.JoymancerJepetto;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player) =>
+		player.CardsPlayedThisMatch
+			.Select(entity => CardUtils.GetProcessedCardFromEntity(entity, player))
+			.Where(card => card is { Type: "Minion" } && (card.Attack == 1 || card.Health == 1))
+			.ToList();
+}
--- Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Priest/RaDen.cs
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Priest/RaDen.cs
@@ -7,12 +7,7 @@ public class RaDen: ICardWithRelatedCards
 {
 	public string GetCardId() => HearthDb.CardIds.Collectible.Priest.RaDen;
 
-	public bool ShouldShowForOpponent(Player opponent)
-	{
-		var card = Database.GetCardFromId(GetCardId());
-		return CardUtils.MayCardBeRelevant(card, Core.Game.CurrentFormat, opponent.OriginalClass) && GetRelatedCards(opponent).Count > 1;
-	}
-
+	public bool ShouldShowForOpponent(Player opponent) => false;
 	public List<Card?> GetRelatedCards(Player player) =>
 		player.CardsPlayedThisMatch
 			.Where(entity => entity.Info.Created && entity.CardId != GetCardId())
