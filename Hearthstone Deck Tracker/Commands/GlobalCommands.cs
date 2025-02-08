3b69f8428cfec10afd438305575d3a8594e3a390
Alex Zeier <alex@hearthsim.net>
2025-02-08 10:47:37 -0800

    refactor: simplify GlobalCommands usage

--- Hearthstone Deck Tracker/Commands/GlobalCommands.cs
+++ Hearthstone Deck Tracker/Commands/GlobalCommands.cs
@@ -3,13 +3,14 @@ using Hearthstone_Deck_Tracker.HsReplay;
 using Hearthstone_Deck_Tracker.Stats;
 
 namespace Hearthstone_Deck_Tracker.Commands;
-	public class GlobalCommands
-	{
-		public static ICommand OpenUrl { get; } = new Command<string>(url => Helper.TryOpenUrl(url));
 
-		public static ICommand OpenReplay { get; } = new Command<GameStats>(game =>
-		{
-			if(game != null)
-				_ = ReplayLauncher.ShowReplay(game, true);
-		});
-	}
+public static class GlobalCommands
+{
+	public static ICommand OpenUrl { get; } = new Command<string>(url => Helper.TryOpenUrl(url));
+
+	public static ICommand OpenReplay { get; } = new Command<GameStats>(game =>
+	{
+		if(game != null)
+			_ = ReplayLauncher.ShowReplay(game, true);
+	});
+}
