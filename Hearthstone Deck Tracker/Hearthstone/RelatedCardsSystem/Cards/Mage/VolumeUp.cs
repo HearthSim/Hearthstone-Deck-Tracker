bb02f645fe69292f6037b790870798be9e4ac2ef
mateuscechetto <mateuscechetto@gmail.com>
2025-03-24 12:59:37 -0300

    fix: volumUp cardId

--- Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Mage/VolumeUp.cs
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Mage/VolumeUp.cs
@@ -4,7 +4,7 @@ namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Mage;
 
 public class VolumeUp : ICardWithHighlight
 {
-	public string GetCardId() => HearthDb.CardIds.Collectible.Mage.WatercolorArtist;
+	public string GetCardId() => HearthDb.CardIds.Collectible.Mage.VolumeUp;
 
 	public HighlightColor ShouldHighlight(Card card, IEnumerable<Card> deck) =>
 		HighlightColorHelper.GetHighlightColor(card.Type == "Spell");
