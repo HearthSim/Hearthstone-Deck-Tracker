16cf69209e6cbd71fbb5a60a79c7ac5a9eceb324
mateuscechetto <mateuscechetto@gmail.com>
2025-04-19 20:55:18 -0300

    feat: add ContrabandStash related cards tooltip

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Rogue/ContrabandStash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Rogue;
+
+public class ContrabandStash: ICardWithRelatedCards
+{
+	public virtual string GetCardId() => HearthDb.CardIds.Collectible.Rogue.ContrabandStash;
+
+	public bool ShouldShowForOpponent(Player opponent)
+	{
+		return false;
+	}
+
+	public List<Card?> GetRelatedCards(Player player) =>
+		player.CardsPlayedThisMatch
+			.Select(entity => CardUtils.GetProcessedCardFromEntity(entity, player))
+			.Where(card => card != null && !card.IsClass(player.CurrentClass) && !card.IsNeutral)
+			.OrderBy(card => card!.Cost)
+			.ToList();
+}
