c408b87e4073ae8661644507fac86ba767f3cb6f
mateuscechetto <mateuscechetto@gmail.com>
2025-04-19 20:52:36 -0300

    feat: add BrilliantMacaw related cards tooltip

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Shaman/BrilliantMacaw.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hearthstone_Deck_Tracker.Utility.Extensions;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Shaman;
+
+public class BrilliantMacaw: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Shaman.BrilliantMacaw;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player)
+	{
+		var lastBattlecry = player.CardsPlayedThisMatch
+										.Select(entity => CardUtils.GetProcessedCardFromEntity(entity, player))
+										.LastOrDefault(card => card is { Mechanics: not null } && card.Mechanics.Contains("Battlecry"));
+		return lastBattlecry != null ? new List<Card?> { lastBattlecry } : new List<Card?>();
+	}
+}
