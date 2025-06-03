31e65c67255bc1e5908ca236dbce9ff41105f1d4
mateuscechetto <mateuscechetto@gmail.com>
2025-06-03 08:18:10 -0300

    feat: add EndbringerUmbra related cards

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Neutral/EndbringerUmbra.cs
@@ -0,0 +1,10 @@
+﻿namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Neutral;
+
+public class EndbringerUmbra: ResurrectionCard
+{
+	public override string GetCardId() => HearthDb.CardIds.Collectible.Neutral.EndbringerUmbra;
+
+	protected override bool FilterCard(Card card) => card.HasDeathrattle();
+
+	protected override bool ResurrectsMultipleCards() => true;
+}
