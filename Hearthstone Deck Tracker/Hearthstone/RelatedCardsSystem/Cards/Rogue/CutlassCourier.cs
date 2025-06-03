df11e1b3aa43a988624a20cc0768e92af49729b7
Benedict Etzel <benedict@hearthsim.net>
2025-06-03 14:23:17 +0200

    feat: implement Cutlass Courier

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Rogue/CutlassCourier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Rogue;
+
+public class CutlassCourier : ICardWithHighlight
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Rogue.CutlassCourier;
+
+	public HighlightColor ShouldHighlight(Card card, IEnumerable<Card> deck) =>
+		HighlightColorHelper.GetHighlightColor(card.IsPirate(), card.Type == "Minion");
+}
