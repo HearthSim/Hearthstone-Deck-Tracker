e41dbc9ecd996f08f510a23ffe4d6c695c73ab06
mateuscechetto <mateuscechetto@gmail.com>
2025-06-26 16:14:01 -0300

    feat: add story of carnassa

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Hunter/StoryOfCarnassa.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Hunter;
+
+public class StoryOfCarnassa : ICardWithRelatedCards
+{
+	private readonly List<Card?> _token = new() {
+		Database.GetCardFromId(HearthDb.CardIds.NonCollectible.Hunter.TheMarshQueen_CarnassasBroodToken)
+	};
+	public string GetCardId() => HearthDb.CardIds.Collectible.Hunter.StoryOfCarnassa;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player) => _token;
+}
