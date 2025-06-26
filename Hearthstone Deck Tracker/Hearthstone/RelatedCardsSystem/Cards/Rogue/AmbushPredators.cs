d206d327b7569057eb362699b783cccc546c9027
Benedict Etzel <benedict@hearthsim.net>
2025-06-26 15:59:51 +0200

    feat: implement Related Card for Ambush Predators

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Rogue/AmbushPredators.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Rogue;
+
+public class AmbushPredators: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Rogue.AmbushPredators;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player) =>
+		new List<Card?>
+		{
+			Database.GetCardFromId(HearthDb.CardIds.NonCollectible.Rogue.AmbushPredators_VenomousSpitterToken),
+		};
+}
