ac40dffd348125acf144ef0f42041499ab90923f
mateuscechetto <mateuscechetto@gmail.com>
2025-06-26 14:55:16 -0300

    feat: add Hybridization

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Druid/Hybridization.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using HearthDb.Enums;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Druid;
+
+public class Hybridization : ICardWithHighlight
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Druid.Hybridization;
+
+	public HighlightColor ShouldHighlight(Card card, IEnumerable<Card> deck) =>
+		HighlightColorHelper.GetHighlightColor(
+			card is {Type: "Minion", Cost: 1 or 4},
+			card is {Type: "Minion", Cost: 2},
+			card is {Type: "Minion", Cost: 3}
+			);
+}
