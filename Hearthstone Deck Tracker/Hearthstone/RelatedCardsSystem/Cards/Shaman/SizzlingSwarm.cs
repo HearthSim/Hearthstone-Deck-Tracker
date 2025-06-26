2624d625d2619dfce4dd3fe73f08227a41029360
Benedict Etzel <benedict@hearthsim.net>
2025-06-26 15:50:53 +0200

    feat: implement Related Cards for Shaman

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Shaman/SizzlingSwarm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Shaman;
+
+public class SizzlingSwarm: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Shaman.SizzlingSwarm;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player) =>
+		new List<Card?>
+		{
+			Database.GetCardFromId(HearthDb.CardIds.Collectible.Neutral.SizzlingCinder),
+		};
+}
---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Shaman/Slagclaw.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Shaman;
+
+public class Slaglaw: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Shaman.Slagclaw;
+
+	public bool ShouldShowForOpponent(Player opponent) => false;
+
+	public List<Card?> GetRelatedCards(Player player) =>
+		new List<Card?>
+		{
+			Database.GetCardFromId(HearthDb.CardIds.Collectible.Neutral.SizzlingCinder),
+			Database.GetCardFromId(HearthDb.CardIds.Collectible.Neutral.SizzlingCinder)
+		};
+}
