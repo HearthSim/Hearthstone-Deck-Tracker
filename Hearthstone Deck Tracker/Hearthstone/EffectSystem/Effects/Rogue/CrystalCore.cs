67bc1050a697e421b5d1ae03f529bd46b3ef2df8
Benedict Etzel <benedict@hearthsim.net>
2025-06-26 20:50:34 +0200

    feat: implement Active Effect for Crystal Core

---
+++ Hearthstone Deck Tracker/Hearthstone/EffectSystem/Effects/Rogue/CrystalCore.cs
@@ -0,0 +1,17 @@
+﻿using Hearthstone_Deck_Tracker.Hearthstone.EffectSystem.Enums;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.EffectSystem.Effects.Rogue;
+
+public class CrystalCoreEnchantment : EntityBasedEffect
+{
+	public override string CardId => HearthDb.CardIds.NonCollectible.Neutral.TheCavernsBelow_CrystallizedTokenUNGORO1;
+	protected override string CardIdToShowInUI => HearthDb.CardIds.NonCollectible.Rogue.TheCavernsBelow_CrystalCoreTokenUNGORO;
+
+	public CrystalCoreEnchantment(int entityId, bool isControlledByPlayer) : base(entityId, isControlledByPlayer)
+	{
+	}
+
+	public override bool UniqueEffect => true;
+	public override EffectDuration EffectDuration => EffectDuration.Permanent;
+	public override EffectTag EffectTag => EffectTag.MinionModification;
+}
