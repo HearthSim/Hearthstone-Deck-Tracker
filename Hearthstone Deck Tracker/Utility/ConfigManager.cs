ea95dd83ad6697966996a9e30084b3c75ebc5a13
IgorMCesar <igormarracho01@gmail.com>
2025-07-03 01:51:57 +0800

    v1.45.2

--- Hearthstone Deck Tracker/Utility/ConfigManager.cs
+++ Hearthstone Deck Tracker/Utility/ConfigManager.cs
@@ -47,7 +47,8 @@ namespace Hearthstone_Deck_Tracker.Utility
 			(new(1, 38, 5), new(1, 38, 6)),
 			(new(1, 38, 8), new(1, 38, 9)),
 			(new(1, 41, 0), new(1, 41, 2)),
-			(new(1, 43, 5), new(1, 43, 6))
+			(new(1, 43, 5), new(1, 43, 6)),
+			(new(1, 45, 1), new(1, 45, 2))
 		};
 		public static bool ShouldShowUpdateNotes()
 		{
