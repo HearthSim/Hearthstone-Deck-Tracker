314ad98955cd2d3812936959b519696a5112b2b8
IgorMCesar <igormarracho01@gmail.com>
2026-01-06 12:11:57 +0000

    feat(BGS): add Blood Gem Barrage Buff Counter

---
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/BgCounters/BloodGemBarrageBuffCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+using Entity = Hearthstone_Deck_Tracker.Hearthstone.Entities.Entity;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.BgCounters;
+
+public class BloodGemBarrageBuffCounter : StatsCounter
+{
+	public override bool IsBattlegroundsCounter => true;
+	protected override string? CardIdToShowInUI => HearthDb.CardIds.NonCollectible.Neutral.BloodGemBarrage;
+	public override string[] RelatedCards => new []
+	{
+		HearthDb.CardIds.NonCollectible.Neutral.BloodGemBarrage
+	};
+
+	public BloodGemBarrageBuffCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	{
+	}
+
+	public override bool ShouldShow() => Game.IsBattlegroundsMatch && (AttackCounter > 0 || HealthCounter > 0);
+
+	public override string[] GetCardsToDisplay() => RelatedCards;
+
+	public override string ValueToShow() => $"+{AttackCounter} / +{HealthCounter}";
+	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
+	{
+		if(!Game.IsBattlegroundsMatch)
+			return;
+
+		if(entity.IsControlledBy(Game.Player.Id) != IsPlayerCounter)
+			return;
+
+		if(entity.Card.Id != HearthDb.CardIds.NonCollectible.Neutral.BloodGemBarrage_BloodGemBarragePlayerEnchDntEnchantment)
+			return;
+
+		if(tag == GameTag.TAG_SCRIPT_DATA_NUM_1)
+			AttackCounter = value;
+
+		if(tag == GameTag.TAG_SCRIPT_DATA_NUM_2)
+			HealthCounter = value;
+	}
+}
