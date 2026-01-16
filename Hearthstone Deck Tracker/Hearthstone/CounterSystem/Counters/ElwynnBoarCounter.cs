3e7ed37bdd62c84a05b121bbdee6c247c93c8ac6
mateuscechetto <mateuscechetto@gmail.com>
2026-01-16 15:34:18 -0300

    feat: add Elwynn Boar Counter

---
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/Counters/ElwynnBoarCounter.cs
@@ -0,0 +1,53 @@
+﻿using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.Counters;
+
+public class ElwynnBoarCounter : NumericCounter
+{
+	protected override string? CardIdToShowInUI => HearthDb.CardIds.Collectible.Neutral.ElwynnBoar;
+
+	public override string[] RelatedCards => new string[]
+	{
+		HearthDb.CardIds.Collectible.Neutral.ElwynnBoar,
+	};
+
+	public ElwynnBoarCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	{
+	}
+
+	public override bool ShouldShow()
+	{
+		if(!Game.IsTraditionalHearthstoneMatch) return false;
+		if(IsPlayerCounter)
+			return InPlayerDeckOrKnown(RelatedCards);
+		return Counter > 0 && OpponentMayHaveRelevantCards();
+	}
+
+	public override string[] GetCardsToDisplay()
+	{
+		return RelatedCards;
+	}
+
+	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
+	{
+		if(!Game.IsTraditionalHearthstoneMatch)
+			return;
+
+		if(!Game.IsMulliganDone)
+			return;
+
+		if(entity.CardId != HearthDb.CardIds.Collectible.Neutral.ElwynnBoar) return;
+
+		if(tag != GameTag.ZONE) return;
+
+		if(prevValue != (int)Zone.PLAY) return;
+
+		if(value != (int)Zone.GRAVEYARD) return;
+
+		var controller = entity.GetTag(GameTag.CONTROLLER);
+		if((controller == Game.Player.Id && IsPlayerCounter) || (controller == Game.Opponent.Id && !IsPlayerCounter))
+			Counter++;
+	}
+}
