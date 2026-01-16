e5f59af274bb7ec9684fa21a1e063a1e771643e7
mateuscechetto <mateuscechetto@gmail.com>
2026-01-16 15:16:19 -0300

    feat: add MagisterDawngrasp related cards

---
+++ Hearthstone Deck Tracker/Hearthstone/RelatedCardsSystem/Cards/Mage/MagisterDawngrasp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HearthDb.Enums;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem.Cards.Mage;
+
+public class MagisterDawngrasp: ICardWithRelatedCards
+{
+	public string GetCardId() => HearthDb.CardIds.Collectible.Mage.MagisterDawngrasp;
+
+	public bool ShouldShowForOpponent(Player opponent)
+	{
+		var card = Database.GetCardFromId(GetCardId());
+		return CardUtils.MayCardBeRelevant(card, Core.Game.CurrentGameType, Core.Game.CurrentFormatType , opponent.OriginalClass) && GetRelatedCards(opponent).Count >= 2;
+	}
+
+	public List<Card?> GetRelatedCards(Player player) =>
+		player.SpellsPlayedCards
+			.Where(entity => entity.HasTag(GameTag.SPELL_SCHOOL))
+			.Select(entity => CardUtils.GetProcessedCardFromEntity(entity, player))
+			.Where(card => card is not null)
+			.Distinct()
+			.OrderBy(c => c?.GetTag(GameTag.SPELL_SCHOOL))
+			.ThenBy(c => c?.Cost)
+			.ToList();
+}
