3824342aa6943a60be72e6e6802f76b8d3379a01
IgorMCesar <igormarracho01@gmail.com>
2026-04-20 19:07:25 +0100

    feat(bgs): add demon fodder counter

---
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/BgCounters/NextRefreshDemonFodderCounter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+using Hearthstone_Deck_Tracker.Utility;
+using Entity = Hearthstone_Deck_Tracker.Hearthstone.Entities.Entity;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.BgCounters;
+
+public class DemonFodderCounter : NumericCounter
+{
+	public override bool IsBattlegroundsCounter => true;
+	protected override string? CardIdToShowInUI => HearthDb.CardIds.NonCollectible.Neutral.LaboratoryAssistant_DemonFodderToken1;
+	public override string[] RelatedCards => new string[]
+	{
+		HearthDb.CardIds.NonCollectible.Neutral.LaboratoryAssistant_DemonFodderToken1,
+		HearthDb.CardIds.NonCollectible.Neutral.LaboratoryAssistant,
+		HearthDb.CardIds.NonCollectible.Neutral.WoodlandDefiler,
+		HearthDb.CardIds.NonCollectible.Neutral.TwistedWrathguard
+
+	};
+
+	public DemonFodderCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	{
+	}
+
+	public override string LocalizedName => LocUtil.Get("Counter_NextRefreshDemonFodder", useCardLanguage: true);
+
+	public override bool ShouldShow() => Game.IsBattlegroundsMatch && Counter > 0;
+
+	public override string[] GetCardsToDisplay() => RelatedCards;
+
+	public override string ValueToShow() => Counter.ToString();
+
+	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
+	{
+		if(!Game.IsBattlegroundsMatch)
+			return;
+
+		if(entity.IsControlledBy(Game.Player.Id) != IsPlayerCounter)
+			return;
+
+		if(entity.GetTag(GameTag.ZONE) == (int)Zone.SETASIDE)
+			return;
+
+		if(tag == GameTag.BACON_FODDERS_IN_REFRESH)
+		{
+			Counter = value;
+			OnCounterChanged();
+		}
+	}
+}
