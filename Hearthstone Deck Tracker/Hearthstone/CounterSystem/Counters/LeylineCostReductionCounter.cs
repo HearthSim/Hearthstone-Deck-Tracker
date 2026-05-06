b70fb3296ea824f874f903d30902e8c5970a5974
mateuscechetto <mateuscechetto@gmail.com>
2026-05-06 00:15:28 -0300

    feat: add leyline counters

---
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/Counters/LeylineCostReductionCounter.cs
@@ -0,0 +1,48 @@
+﻿using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+using Hearthstone_Deck_Tracker.Utility;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.Counters;
+
+public class LeylineCostReductionCounter : NumericCounter
+{
+	protected override string? CardIdToShowInUI => HearthDb.CardIds.Collectible.Mage.LeyWalker;
+	public override string[] RelatedCards => new string[]
+	{
+		HearthDb.CardIds.Collectible.Mage.LeyWalker,
+		HearthDb.CardIds.Collectible.Mage.TheArcanomicon,
+	};
+
+	public LeylineCostReductionCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	{
+	}
+
+	public override bool ShouldShow() => Game.IsTraditionalHearthstoneMatch && Counter > 0;
+
+	public override string[] GetCardsToDisplay()
+	{
+		return RelatedCards;
+	}
+
+	public override string ValueToShow() => Counter.ToString();
+	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
+	{
+		if(!Game.IsTraditionalHearthstoneMatch)
+			return;
+
+		if(entity.IsControlledBy(Game.Player.Id) != IsPlayerCounter)
+			return;
+
+		if(tag != GameTag.ZONE)
+			return;
+
+		if(value != (int)Zone.PLAY)
+			return;
+
+		if(entity.Card.Id == HearthDb.CardIds.NonCollectible.Mage.LeyWalker_UnblockLeylineToken)
+		{
+			Counter += 1;
+		}
+	}
+}
---
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/Counters/LeylineEffectIncreaseCounter.cs
@@ -0,0 +1,51 @@
+using HearthDb.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.Counters;
+
+public class LeylineEffectIncreaseCounter : NumericCounter
+{
+	protected override string? CardIdToShowInUI => HearthDb.CardIds.Collectible.Mage.MysticRunesaber;
+	public override string[] RelatedCards => new string[]
+	{
+		HearthDb.CardIds.Collectible.Mage.MysticRunesaber,
+		HearthDb.CardIds.Collectible.Mage.TheArcanomicon,
+	};
+
+	public LeylineEffectIncreaseCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	{
+	}
+
+	public override bool ShouldShow()
+	{
+		if(!Game.IsTraditionalHearthstoneMatch) return false;
+		return Counter > 0;
+	}
+
+	public override string[] GetCardsToDisplay()
+	{
+		return RelatedCards;
+	}
+
+	public override string ValueToShow() => Counter.ToString();
+
+	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
+	{
+		if(!Game.IsTraditionalHearthstoneMatch)
+			return;
+
+		if((int)tag != 4288)
+			return;
+
+		if(value == 0)
+			return;
+
+		var controller = entity.GetTag(GameTag.CONTROLLER);
+		var isPlayerController = controller == Game.Player.Id;
+		if(isPlayerController != IsPlayerCounter)
+			return;
+
+		Counter = value;
+	}
+}
