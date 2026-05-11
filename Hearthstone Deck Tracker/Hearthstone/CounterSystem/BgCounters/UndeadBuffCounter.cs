41531d717ee43deb6b06c94c54313ae1cf6dde87
Benedict Etzel <benedict@hearthsim.net>
2026-05-11 12:47:53 +0200

    feat: add health buff to BGS Undead buff counter

--- Hearthstone Deck Tracker/Hearthstone/CounterSystem/BgCounters/UndeadBuffCounter.cs
+++ Hearthstone Deck Tracker/Hearthstone/CounterSystem/BgCounters/UndeadBuffCounter.cs
@@ -7,19 +7,19 @@ using Entity = Hearthstone_Deck_Tracker.Hearthstone.Entities.Entity;
 
 namespace Hearthstone_Deck_Tracker.Hearthstone.CounterSystem.BgCounters;
 
-public class UndeadAttackBonusCounter : NumericCounter
+public class UndeadBuffCounter : StatsCounter
 {
 	public override bool IsBattlegroundsCounter => true;
 	protected override string? CardIdToShowInUI => HearthDb.CardIds.NonCollectible.Neutral.NerubianDeathswarmer;
 	public override string LocalizedName => LocUtil.Get("Counter_UndeadAttackBonus", useCardLanguage: true);
 	public override string[] RelatedCards => new string[] {};
 
-	public UndeadAttackBonusCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
+	public UndeadBuffCounter(bool controlledByPlayer, GameV2 game) : base(controlledByPlayer, game)
 	{
 	}
 
 	public override bool ShouldShow() => Game.IsBattlegroundsMatch
-	                                     && Counter > 0
+	                                     && (AttackCounter > 0 || HealthCounter > 0)
 	                                     && Game.Player.Board.Any(e => e.Card.IsUndead());
 
 	public override string[] GetCardsToDisplay()
@@ -28,15 +28,15 @@ public class UndeadAttackBonusCounter : NumericCounter
 		{
 			HearthDb.CardIds.NonCollectible.Neutral.NerubianDeathswarmer,
 			HearthDb.CardIds.NonCollectible.Neutral.AnubarakNerubianKing,
+			HearthDb.CardIds.NonCollectible.Neutral.DustboneDevastator,
+			HearthDb.CardIds.NonCollectible.Neutral.Plaguerunner,
 			HearthDb.CardIds.NonCollectible.Neutral.ChampionOfThePrimus,
 			HearthDb.CardIds.NonCollectible.Neutral.Butchering,
 			HearthDb.CardIds.NonCollectible.Neutral.ForsakenWeaver,
-			HearthDb.CardIds.NonCollectible.Neutral.DustboneDevastator,
-			HearthDb.CardIds.NonCollectible.Neutral.Plaguerunner,
 		};
 	}
 
-	public override string ValueToShow() => $"+{Counter}";
+	public override string ValueToShow() => HealthCounter > 0 ? $"+{AttackCounter} / +{HealthCounter}" : $"+{AttackCounter}";
 	public override void HandleTagChange(GameTag tag, IHsGameState gameState, Entity entity, int value, int prevValue)
 	{
 		if(!Game.IsBattlegroundsMatch)
@@ -57,7 +57,9 @@ public class UndeadAttackBonusCounter : NumericCounter
 
 
 		if(tag == GameTag.TAG_SCRIPT_DATA_NUM_1)
-			Counter = value;
+			AttackCounter = value;
 
+		if(tag == GameTag.TAG_SCRIPT_DATA_NUM_2)
+			HealthCounter = value;
 	}
 }
