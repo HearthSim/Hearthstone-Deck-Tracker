90fbc30e2d438bcf875d4efcb800179cd030b6d7
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-07-01 12:32:07 +0200

    TEMP COMMIT TO FIX BUILD PIPELINE

--- SignToolShim/Program.cs
+++ SignToolShim/Program.cs
@@ -20,6 +20,12 @@ namespace SignToolShim
 				return 0;
 			}
 
+			if (!HasSigningConfiguration())
+			{
+				Console.WriteLine($"[SignToolShim] Skipping signing because DigiCert Software Trust Manager environment variables are not configured: {filePath}");
+				return 0;
+			}
+
 			var smctlArgs = $"sign --simple --keypair-alias=key_1409653344 --input=\"{filePath}\"";
 			var processInfo = new ProcessStartInfo
 			{
@@ -39,6 +45,14 @@ namespace SignToolShim
 				process.WaitForExit();
 				return process.ExitCode;
 			}
+		}
+
+		private static bool HasSigningConfiguration()
+		{
+			return !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("SM_HOST"))
+				&& !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("SM_API_KEY"))
+				&& !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("SM_CLIENT_CERT_FILE"))
+				&& !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("SM_CLIENT_CERT_PASSWORD"));
 		}
 	}
 }
