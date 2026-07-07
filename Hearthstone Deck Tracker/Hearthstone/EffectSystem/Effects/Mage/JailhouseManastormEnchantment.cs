0028db64890c7a8bf5d369cb0fc1abc5cd497d1b
mateuscechetto <mateuscechetto@gmail.com>
2026-07-07 15:51:56 -0300

    feat: add JailhouseManastormEnchantment

---
+++ Hearthstone Deck Tracker/Hearthstone/EffectSystem/Effects/Mage/JailhouseManastormEnchantment.cs
@@ -0,0 +1,16 @@
+﻿using Hearthstone_Deck_Tracker.Hearthstone.EffectSystem.Enums;
+
+namespace Hearthstone_Deck_Tracker.Hearthstone.EffectSystem.Effects.Mage;
+
+public class JailhouseManastormEnchantment : EntityBasedEffect
+{
+	public override string CardId => HearthDb.CardIds.NonCollectible.Mage.JailhouseManastorm_ManastormSummoningEnchantment;
+	protected override string CardIdToShowInUI => HearthDb.CardIds.Collectible.Mage.JailhouseManastorm;
+
+	public JailhouseManastormEnchantment(int entityId, bool isControlledByPlayer) : base(entityId, isControlledByPlayer)
+	{
+	}
+
+	public override EffectDuration EffectDuration => EffectDuration.Permanent;
+	public override EffectTag EffectTag => EffectTag.Summon;
+}
