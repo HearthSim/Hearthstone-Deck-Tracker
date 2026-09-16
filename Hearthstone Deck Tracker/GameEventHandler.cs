51cfd5e2b92ccf80b39b2575d984918006443b8f
Benedict Etzel <benedict@hearthsim.net>
2026-09-16 17:36:55 +0200

    chore: drop unnecessary metric

--- Hearthstone Deck Tracker/GameEventHandler.cs
+++ Hearthstone Deck Tracker/GameEventHandler.cs
@@ -801,7 +801,6 @@ namespace Hearthstone_Deck_Tracker
 				if(_game.CurrentGameMode == Spectator && _game.CurrentGameStats.Result == GameResult.None)
 				{
 					Log.Info("Game was spectator mode without a game result. Probably exited spectator mode early.");
-					SentryReporter.DropBattlegroundsEvents("spectator_no_result");
 					return;
 				}
 				var player = _game.Entities.FirstOrDefault(e => e.Value?.IsPlayer ?? false).Value;
