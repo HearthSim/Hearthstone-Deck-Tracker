5e1ce9cfe010422e6051c7409bee811a1bd406dd
Benedict Etzel <benedict@hearthsim.net>
2026-09-16 20:27:18 +0200

    fix: apply Battlegrounds tag overrides to card races

--- Hearthstone Deck Tracker/Hearthstone/BattlegroundsDb.cs
+++ Hearthstone Deck Tracker/Hearthstone/BattlegroundsDb.cs
@@ -34,31 +34,75 @@ public class BattlegroundsDb
 		};
 	}
 
-	private void Update(List<RemoteData.TagOverride>? tagOverrides)
+	private class TagLookup
 	{
-		var overrides = new Dictionary<int, Tuple<GameTag, int>>();
-		if (tagOverrides != null)
+		private readonly Dictionary<(int DbfId, GameTag Tag), int> _overrides = new();
+
+		public TagLookup(List<RemoteData.TagOverride>? tagOverrides)
 		{
+			if(tagOverrides == null)
+				return;
 			foreach(var tagOverride in tagOverrides)
-				overrides[tagOverride.DbfId] = new Tuple<GameTag, int>(tagOverride.Tag, tagOverride.Value);
+				_overrides[(tagOverride.DbfId, tagOverride.Tag)] = tagOverride.Value;
 		}
 
-		int GetTag(HearthDb.Card card, GameTag tag)
+		public int GetTag(HearthDb.Card card, GameTag tag)
+			=> _overrides.TryGetValue((card.DbfId, tag), out var value) ? value : card.Entity.GetTag(tag);
+
+		public Race GetRace(HearthDb.Card card) => (Race)GetTag(card, GameTag.CARDRACE);
+
+		// HearthDb resolves the secondary race from a per-race marker tag being present at all, so an
+		// override can only remove one by setting that tag to 0
+		public Race GetSecondaryRace(HearthDb.Card card)
 		{
-			if(overrides.TryGetValue(card.DbfId, out var tagOverride) && tagOverride.Item1 == tag) return tagOverride.Item2;
-			return card.Entity.GetTag(tag);
+			var race = GetRace(card);
+			foreach(var tag in card.Entity.Tags)
+			{
+				if(!RaceUtils.TagRaceMap.TryGetValue(tag.EnumId, out var secondaryRace) || secondaryRace == race)
+					continue;
+				if(_overrides.TryGetValue((card.DbfId, (GameTag)tag.EnumId), out var value) && value == 0)
+					continue;
+				return secondaryRace;
+			}
+			return Race.INVALID;
 		}
+	}
+
+	private IEnumerable<Race> GetRaces(HearthDb.Card card, TagLookup tags)
+	{
+		var race = tags.GetRace(card);
+		if(race == Race.INVALID)
+		{
+			var racesInText = Races
+				.Where(x => x != Race.ALL && x != Race.INVALID)
+				.Where(x => card.GetLocText(Locale.enUS)?.Contains(HearthDbConverter.RaceConverter(x)) ?? false)
+				.ToList();
+			if(racesInText.Count == 1)
+			{
+				yield return racesInText.Single();
+				yield break;
+			}
+		}
+		yield return race;
+		var secondaryRace = tags.GetSecondaryRace(card);
+		if(secondaryRace != Race.INVALID)
+			yield return secondaryRace;
+	}
+
+	private void Update(List<RemoteData.TagOverride>? tagOverrides)
+	{
+		var tags = new TagLookup(tagOverrides);
 
 		var baconCards = Cards.All.Values
 			.Where(x =>
-				GetTag(x, GameTag.TECH_LEVEL) > 0
+				tags.GetTag(x, GameTag.TECH_LEVEL) > 0
 				// explicitly check for == 1, as Rot Hide Gnoll has 2 but is not in the pool
-				&& GetTag(x, GameTag.IS_BACON_POOL_MINION) == 1
+				&& tags.GetTag(x, GameTag.IS_BACON_POOL_MINION) == 1
 			)
 			.ToList();
 
 		Races.Clear();
-		foreach(var race in baconCards.Select(x => x.Race))
+		foreach(var race in baconCards.Select(tags.GetRace))
 			Races.Add(race);
 
 		_cardsByTier.Clear();
@@ -66,8 +110,8 @@ public class BattlegroundsDb
 		_duosExclusiveCardsByTier.Clear();
 		foreach(var card in baconCards)
 		{
-			var tier = GetTag(card, GameTag.TECH_LEVEL);
-			var duosExclusive = GetTag(card, GameTag.IS_BACON_DUOS_EXCLUSIVE);
+			var tier = tags.GetTag(card, GameTag.TECH_LEVEL);
+			var duosExclusive = tags.GetTag(card, GameTag.IS_BACON_DUOS_EXCLUSIVE);
 			// the game doesn't actually set this ever to a negative value, but we use that as a sentinel
 			// value to hide Solos-exclusive cards in Duos
 			var targetDict = (
@@ -77,7 +121,7 @@ public class BattlegroundsDb
 			);
 			if(!targetDict.ContainsKey(tier))
 				targetDict[tier] = new Dictionary<Race, List<Card>>();
-			foreach(var race in new HashSet<Race>(GetRaces(card)))
+			foreach(var race in new HashSet<Race>(GetRaces(card, tags)))
 			{
 				if(!targetDict[tier].ContainsKey(race))
 					targetDict[tier][race] = new List<Card>();
@@ -91,14 +135,14 @@ public class BattlegroundsDb
 		_duosExclusiveSpellsByTier.Clear();
 		_spells.AddRange(Cards.All.Values
 			.Where(x => (
-				GetTag(x, GameTag.TECH_LEVEL) > 0
+				tags.GetTag(x, GameTag.TECH_LEVEL) > 0
 				&& x.Type == CardType.BATTLEGROUND_SPELL
-				&& GetTag(x, GameTag.IS_BACON_POOL_SPELL) == 1
+				&& tags.GetTag(x, GameTag.IS_BACON_POOL_SPELL) == 1
 			)));
 		foreach(var card in _spells)
 		{
-			var tier = GetTag(card, GameTag.TECH_LEVEL);
-			var duosExclusive = GetTag(card, GameTag.IS_BACON_DUOS_EXCLUSIVE);
+			var tier = tags.GetTag(card, GameTag.TECH_LEVEL);
+			var duosExclusive = tags.GetTag(card, GameTag.IS_BACON_DUOS_EXCLUSIVE);
 			var targetDict = (
 				duosExclusive > 0 ? _duosExclusiveSpellsByTier :
 				duosExclusive < 0 ? _solosExclusiveSpellsByTier :
@@ -113,11 +157,11 @@ public class BattlegroundsDb
 		_buddiesByTier.Clear();
 		_solosExclusiveBuddiesByTier.Clear();
 		_duosExclusiveBuddiesByTier.Clear();
-		_buddies.AddRange(Cards.All.Values.Where(x => GetTag(x, GameTag.BACON_BUDDY) == 1 && GetTag(x, GameTag.BACON_TRIPLED_BASE_MINION_ID) == 0));
+		_buddies.AddRange(Cards.All.Values.Where(x => tags.GetTag(x, GameTag.BACON_BUDDY) == 1 && tags.GetTag(x, GameTag.BACON_TRIPLED_BASE_MINION_ID) == 0));
 		foreach(var card in _buddies)
 		{
-			var tier = GetTag(card, GameTag.TECH_LEVEL);
-			var duosExclusive = GetTag(card, GameTag.IS_BACON_DUOS_EXCLUSIVE);
+			var tier = tags.GetTag(card, GameTag.TECH_LEVEL);
+			var duosExclusive = tags.GetTag(card, GameTag.IS_BACON_DUOS_EXCLUSIVE);
 			var targetDict = (
 				duosExclusive > 0 ? _duosExclusiveBuddiesByTier :
 				duosExclusive < 0 ? _solosExclusiveBuddiesByTier :
@@ -127,25 +171,6 @@ public class BattlegroundsDb
 				targetDict[tier] = new List<Card>();
 			targetDict[tier].Add(new Card(card, true));
 		}
-	}
-
-	private IEnumerable<Race> GetRaces(HearthDb.Card card)
-	{
-		if(card.Race == Race.INVALID)
-		{
-			var racesInText = Races
-				.Where(x => x != Race.ALL && x != Race.INVALID)
-				.Where(x => card.GetLocText(Locale.enUS)?.Contains(HearthDbConverter.RaceConverter(x)) ?? false)
-				.ToList();
-			if(racesInText.Count == 1)
-			{
-				yield return racesInText.Single();
-				yield break;
-			}
-		}
-		yield return card.Race;
-		if(card.SecondaryRace != Race.INVALID)
-			yield return card.SecondaryRace;
 	}
 
 	public List<Card> GetCards(int tier, Race race, bool isDuos)
